34504cf7b21bdd2de68ea281668672f70135a09f
harukaeru <nekoneko.myaomyao@gmail.com>
2021-09-11 07:12:47 +0900

    2021/09/10

--- abc068/C/main2.py
+++ abc068/C/main2.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 from collections import defaultdict, deque
 
-G = defaultdict(list)
+YES = "POSSIBLE"  # type: str
+NO = "IMPOSSIBLE"  # type: str
+
 N, M = map(int, input().split())
+G = defaultdict(list)
+
 for i in range(M):
     a, b = map(int, input().split())
     a, b = a - 1, b - 1
     G[a].append(b)
     G[b].append(a)
 
-befores = [None] * N
 
 q = deque()
 q.append(0)
+
+distances = [None] * N
+distances[0] = 0
 
 while len(q) > 0:
     n = q.popleft()
 
     for next_node in G[n]:
-        if befores[next_node] is None:
-            befores[next_node] = n
+        if distances[next_node] is None:
+            distances[next_node] = distances[n] + 1
             q.append(next_node)
 
 
-# print('befores', befores)
-
-for i in befores[1:]:
-    if i is None:
-        print('No')
-        exit()
-
-print('Yes')
-for i in befores[1:]:
-    print(i + 1)
+ok = distances[N - 1] == 2
+if ok:
+    print(YES)
+else:
+    print(NO)
