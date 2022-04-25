d613a2b924b5dd6056f559ef676089f144a6ea42
harukaeru <nekoneko.myaomyao@gmail.com>
2022-04-25 09:04:54 +0900

    2022/04/25

--- apg4b/EX16/main.cpp
+++ apg4b/EX16/main.cpp
@@ -5,5 +5,22 @@
 using namespace std;
 
 int main() {
+  vector<int> data(5);
+
+  rep(i, 5) {
+    cin >> data.at(i);
+  }
+
+  rep(i, 4) {
+    int current_value = data.at(i);
+    int next_value = data.at(i + 1);
+    if (current_value == next_value) {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+
+  cout << "NO" << endl;
+
   return 0;
 }
