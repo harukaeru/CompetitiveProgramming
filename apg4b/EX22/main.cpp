15a5faeb99274c8fa3994cf57121ca25e16d5fda
harukaeru <nekoneko.myaomyao@gmail.com>
2022-05-04 01:15:03 +0900

    2022/05/04

--- apg4b/EX22/main.cpp
+++ apg4b/EX22/main.cpp
@@ -5,5 +5,24 @@
 using namespace std;
 
 int main() {
+  int N;
+  cin >> N;
+
+  vector<pair<int, int>> p_list;
+
+  rep(i, N) {
+    pair<int, int> a;
+    cin >> a.second >> a.first;
+    p_list.push_back(a);
+  }
+
+  sort(p_list.begin(), p_list.end());
+
+  rep(i, N) {
+    int a, b;
+    tie(b, a) = p_list.at(i);
+    cout << a << " " << b << endl;
+  }
+
   return 0;
 }
--- apg4b/EX23/main.cpp
+++ apg4b/EX23/main.cpp
@@ -5,5 +5,29 @@
 using namespace std;
 
 int main() {
+  int N;
+  cin >> N;
+
+  map<int, int> m;
+  int max_count = 0;
+  int n_which_is_mode = -1;
+
+  rep(i, N) {
+    int n;
+    cin >> n;
+    if (m.count(n)) {
+      ++m.at(n);
+    } else {
+      m[n] = 1;
+    }
+
+    if (m.at(n) > max_count) {
+      n_which_is_mode = n;
+      max_count = m.at(n);
+    }
+  }
+
+  cout << n_which_is_mode << " " << max_count << endl;
+
   return 0;
 }
