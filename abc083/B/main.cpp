b7bce851538028e2007d41241718013582735ebc
harukaeru <nekoneko.myaomyao@gmail.com>
2022-05-17 10:29:26 +0900

    2022/05/17

--- abc083/B/main.cpp
+++ abc083/B/main.cpp
@@ -5,5 +5,23 @@
 using namespace std;
 
 int main() {
+  int N, A, B;
+  cin >> N >> A >> B;
+
+  int t = 0;
+  rep(i, N) {
+    int j = i + 1;
+    int s = 0;
+    while (j > 0) {
+      s += j % 10;
+      j /= 10;
+    }
+
+    if (A <= s && s <= B) {
+      t += (i + 1);
+    }
+  }
+
+  cout << t << endl;
   return 0;
 }
--- abc084/B/main.cpp
+++ abc084/B/main.cpp
@@ -5,5 +5,29 @@
 using namespace std;
 
 int main() {
+  int A, B;
+  cin >> A >> B;
+  string S;
+  cin >> S;
+  rep(i, A + B + 1) {
+    if (i >= (int)S.size()) {
+      cout << "No" << endl;
+      return 0;
+    }
+
+    if (i == A) {
+      if (S.at(i) != '-') {
+        cout << "No" << endl;
+        return 0;
+      }
+    } else {
+      int l = S.at(i) - '0';
+      if (l < 0 || 9 < l) {
+        cout << "No" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "Yes" << endl;
   return 0;
 }
