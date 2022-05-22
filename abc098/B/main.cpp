837bddc88cf2de3c4d52fa2828d9360e7808a1d1
harukaeru <nekoneko.myaomyao@gmail.com>
2022-05-22 22:33:07 +0900

    2022/05/22

--- abc098/B/main.cpp
+++ abc098/B/main.cpp
@@ -1,9 +1,41 @@
 #include <bits/stdc++.h>
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-
 using namespace std;
 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define pp(v)              \
+  for (auto x : v) {       \
+    std::cout << x << ' '; \
+  }                        \
+  std::cout << endl;
+
+set<char> get_set(string s) {
+  set<char> r;
+  rep(i, s.size()) {
+    r.insert(s.at(i));
+  }
+  return r;
+}
+
 int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  int m = 0;
+  // カット位置
+  for (int i = 1; i < N - 1; i++) {
+    string left = S.substr(0, i);
+    string right = S.substr(i, N - i);
+
+    auto l = get_set(left);
+    auto r = get_set(right);
+    set<char> c;
+
+    set_intersection(l.begin(), l.end(), r.begin(), r.end(), inserter(c, c.begin()));
+
+    m = max(m, (int)c.size());
+  }
+  cout << m << endl;
+
   return 0;
 }
--- abc233/C/main.cpp
+++ abc233/C/main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-
 using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define pp(v)              \
+  for (auto x : v) {       \
+    std::cout << x << ' '; \
+  }                        \
+  std::cout << endl;
 
 int main() {
   return 0;
