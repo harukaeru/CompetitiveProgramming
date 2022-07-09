53e30dbe36811f64dcf4430f934846e7a57df652
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-09 12:52:37 +0900

    2022/07/09

--- abc168/B/main.cpp
+++ abc168/B/main.cpp
@@ -10,5 +10,16 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int K;
+  cin >> K;
+
+  string S;
+  cin >> S;
+
+  if ((int)(S.size()) <= K) {
+    cout << S << endl;
+  } else {
+    cout << S.substr(0, K) << "..." << endl;
+  }
   return 0;
 }
--- abc169/B/main.cpp
+++ abc169/B/main.cpp
@@ -10,5 +10,30 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  long long o = 1e18;
+  int N;
+  cin >> N;
+
+  vector<long long> A(N);
+  rep(i, N) {
+    cin >> A.at(i);
+    if (A.at(i) == 0) {
+      cout << 0 << endl;
+      return 0;
+    }
+  }
+
+  long long ans = 1;
+  rep(i, N) {
+    long long a = A.at(i);
+    if (o / ans >= a) {
+      ans *= a;
+    } else {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+
+  cout << ans << endl;
   return 0;
 }
