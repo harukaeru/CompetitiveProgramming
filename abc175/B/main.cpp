7bc111d998eafd05977754d13a88b1eeb9811a19
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-10 01:38:42 +0900

    2022/07/10

--- abc175/B/main.cpp
+++ abc175/B/main.cpp
@@ -10,5 +10,31 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N;
+  cin >> N;
+
+  vector<long> L(N);
+  rep(i, N) {
+    cin >> L.at(i);
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < N - 2; i++) {
+    long Li = L.at(i);
+    for (int j = i + 1; j < N - 1; j++) {
+      long Lj = L.at(j);
+      for (int k = j + 1; k < N; k++) {
+        long Lk = L.at(k);
+        if (Li != Lj && Lj != Lk && Lk != Li) {
+          long maxL = max({Li, Lj, Lk});
+          long rest = Li + Lj + Lk - maxL;
+          if (maxL < rest) {
+            cnt++;
+          }
+        }
+      }
+    }
+  }
+  cout << cnt << endl;
   return 0;
 }
