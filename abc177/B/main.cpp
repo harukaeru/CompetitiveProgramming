b990e7c2b3b5c16e9edfe126c1305e395abdef46
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-13 01:46:53 +0900

    2022/07/13

--- abc177/B/main.cpp
+++ abc177/B/main.cpp
@@ -10,5 +10,22 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  string S, T;
+  cin >> S >> T;
+
+  int min_count = 999999;
+  for (int i = 0; i < (int)(S.size()) - (int)T.size() + 1; i++) {
+    int count = 0;
+    for (int j = 0; j < (int)(T.size()); j++) {
+      char s = S.at(j + i);
+      char t = T.at(j);
+      if (s != t) {
+        count++;
+      }
+    }
+    min_count = min(min_count, count);
+  }
+
+  cout << min_count << endl;
   return 0;
 }
--- abc178/B/main.cpp
+++ abc178/B/main.cpp
@@ -10,5 +10,9 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  long a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << max(max(a * c, a * d), max(b * c, b * d)) << endl;
+
   return 0;
 }
