5a798a50263ff27ffca310cc8c8704f81df3d288
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-14 22:25:28 +0900

    2022/07/14

--- abc183/B/main.cpp
+++ abc183/B/main.cpp
@@ -10,5 +10,10 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int sx, sy, gx, gy;
+  cin >> sx >> sy >> gx >> gy;
+
+  cout << setprecision(10) << (double)sx + (double)(gx - sx) / (gy + sy) * sy << endl;
+
   return 0;
 }
