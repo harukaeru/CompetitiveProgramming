9ae900be12a3ea84ff3b27dd5e0f066b91357ca3
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-28 01:22:56 +0900

    2022/07/28

--- abc185/B/main.cpp
+++ abc185/B/main.cpp
@@ -10,5 +10,34 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N, M, T;
+  cin >> N >> M >> T;
+  vector<int> A(M);
+  vector<int> B(M);
+  rep(i, M) {
+    cin >> A.at(i);
+    cin >> B.at(i);
+  }
+
+  int battery = N;
+  int current_time = 0;
+  rep(i, M) {
+    battery -= (A.at(i) - current_time);
+    if (battery <= 0) {
+      cout << "No" << endl;
+      return 0;
+    }
+    battery = min(battery + (B.at(i) - A.at(i)), N);
+    current_time = B.at(i);
+  }
+
+  battery -= T - B.at(M - 1);
+  if (battery <= 0) {
+    cout << "No" << endl;
+    return 0;
+  }
+
+  cout << "Yes" << endl;
+
   return 0;
 }
