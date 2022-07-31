e22e8e7a94402c08ee2a365943c0601235274a7b
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-31 23:22:17 +0900

    2022/07/31

--- abc187/B/main.cpp
+++ abc187/B/main.cpp
@@ -10,5 +10,28 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<pair<int, int>> a(N);
+
+  rep(i, N) {
+    int x, y;
+    cin >> x >> y;
+    a.at(i) = make_pair(x, y);
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < N - 1; i++) {
+    auto left = a.at(i);
+    for (int j = i + 1; j < N; j++) {
+      auto right = a.at(j);
+
+      double slope = (double)(right.second - left.second) / (right.first - left.first);
+      if (-1 <= slope && slope <= 1) {
+        cnt++;
+      }
+    }
+  }
+  cout << cnt << endl;
   return 0;
 }
