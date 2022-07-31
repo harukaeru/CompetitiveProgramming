1249514f6e0882eef6da6008c7e093f4a521485e
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-31 23:24:56 +0900

    2022/07/31 2

--- abc188/B/main.cpp
+++ abc188/B/main.cpp
@@ -10,5 +10,27 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  vector<int> B(N);
+  rep(i, N) {
+    cin >> A.at(i);
+  }
+  rep(i, N) {
+    cin >> B.at(i);
+  }
+
+  int total = 0;
+  rep(i, N) {
+    total += A.at(i) * B.at(i);
+  }
+
+  if (total == 0) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
   return 0;
 }
