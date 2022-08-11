4960e2aa9b5e73c29d56358633d586076fdaa3d9
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-11 09:40:19 +0900

    2022/08/11

--- abc192/B/main.cpp
+++ abc192/B/main.cpp
@@ -10,5 +10,25 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  string S;
+  cin >> S;
+
+  rep(i, S.size()) {
+    char s = S.at(i);
+
+    if (i % 2 == 1) {
+      if (!isupper(s)) {
+        cout << "No" << endl;
+        return 0;
+      }
+    } else {
+      if (!islower(s)) {
+        cout << "No" << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "Yes" << endl;
   return 0;
 }
