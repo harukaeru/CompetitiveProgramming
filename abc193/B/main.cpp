3a77e85e0999f8afda67213c6d2186e195756322
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-11 16:45:55 +0900

    2022/08/11

--- abc193/B/main.cpp
+++ abc193/B/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <math.h>
 
 using namespace std;
 
@@ -10,5 +11,27 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N;
+  cin >> N;
+
+  int found = false;
+  int min_price = pow(10, 9) + 1;
+  rep(i, N) {
+    int a, p, x;
+    cin >> a >> p >> x;
+
+    int inventory = max(x - a, 0);
+    if (inventory > 0) {
+      found = true;
+      min_price = min(min_price, p);
+    }
+  }
+
+  if (!found) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  cout << min_price << endl;
   return 0;
 }
--- abc194/B/main.cpp
+++ abc194/B/main.cpp
@@ -10,5 +10,52 @@ using namespace std;
   std::cout << endl;
 
 int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  vector<int> B(N);
+  rep(i, N) {
+    cin >> A.at(i) >> B.at(i);
+  }
+
+  int min_a = 99999999;
+  int i_when_min_a = -1;
+
+  int min_b = 99999999;
+  int i_when_min_b = -1;
+
+  for (int i = 0; i < N; i++) {
+    int a = A.at(i);
+    int b = B.at(i);
+
+    if (min_a > a) {
+      i_when_min_a = i;
+      min_a = a;
+    }
+    if (min_b > b) {
+      i_when_min_b = i;
+      min_b = b;
+    }
+  }
+
+  int min_total = 9999999;
+  for (int i = 0; i < N; i++) {
+    int a = A.at(i);
+    int b = B.at(i);
+
+    if (i != i_when_min_a) {
+      min_total = min(min_total, max(min_a, b));
+    } else {
+      min_total = min(min_total, min_a + b);
+    }
+    if (i != i_when_min_b) {
+      min_total = min(min_total, max(a, min_b));
+    } else {
+      min_total = min(min_total, a + min_b);
+    }
+  }
+
+  cout << min_total << endl;
   return 0;
 }
