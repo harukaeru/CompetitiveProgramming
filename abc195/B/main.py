3d011fb1b73f8ad033152ffbd51f4ef2455f1dd3
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-12 01:08:14 +0900

    2022/08/12

--- abc195/B/main.py
+++ abc195/B/main.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+A, B, W = map(int, input().split())
+
+mi = -1
+ma = -1
+# もっとも少ない個数
+min_count = W * 1000 // B
+# 一致すればそのまま。一致しなければ足りないのでそれより1個多いハズ
+if min_count == W * 1000 / B:
+  mi = min_count
+else:
+  mi = min_count + 1
+
+max_count = W * 1000 // A
+# 一致すればそのまま。一致しなくてもその個数まで実数を駆使して小さくすればいい
+if max_count == W * 1000 / A:
+  ma = max_count
+else:
+  ma = max_count
+
+if (mi > ma):
+  print('UNSATISFIABLE')
+  exit()
+
+print(mi, ma)
