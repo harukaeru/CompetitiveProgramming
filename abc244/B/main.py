1c4762a530bf2c2c6ab68bc7c0bde2adbea7419b
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-13 19:56:17 +0900

    2022/08/13

--- abc244/B/main.py
+++ abc244/B/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import operator
+N = int(input())
+S = input()
+
+directions = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+current_direction_idx = 0
+
+pos = (0, 0)
+for s in S:
+  if s == 'S':
+    direction = directions[current_direction_idx]
+    pos = tuple(map(operator.add, pos, direction))
+  else:
+    current_direction_idx = (current_direction_idx + 1) % 4
+print(*pos)
