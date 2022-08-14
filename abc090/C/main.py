2b5de9bca478a2df4c6b95a622deda90db41d3ed
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-14 23:26:17 +0900

    2022/08/14

--- abc090/C/main.py
+++ abc090/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+x = list(map(int, input().split()))
+x.sort()
+x = tuple(x)
+
+if x == (1, 1):
+  print(1)
+  exit()
+
+if x[0] == 1:
+  print(x[1] - 2)
+  exit()
+
+print((x[0] - 2) * (x[1] - 2))
--- abc091/C/main.py
+++ abc091/C/main.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+N = int(input())
+
+reds = []
+for i in range(N):
+  red = tuple(map(int, input().split()))
+  reds.append(red)
+
+blues = []
+for i in range(N):
+  blue = tuple(map(int, input().split()))
+  blues.append(blue)
+
+blues.sort()
+
+cnt = 0
+# from pprint import pprint
+# print('----reds')
+# pprint(reds)
+# print('----blues')
+# pprint(blues)
+for blue in blues:
+  red_to_remove = None
+  max_v = -1
+  for red in reds:
+    if blue[0] >= red[0] and blue[1] >= red[1]:
+      if max_v < red[1]:
+        max_v = red[1]
+        red_to_remove = red
+  if red_to_remove is not None:
+    reds.remove(red_to_remove)
+    cnt += 1
+
+print(cnt)
