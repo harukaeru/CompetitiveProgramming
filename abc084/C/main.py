ce420adf670a4839caf585483a4c4fc3fd09df20
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-14 18:59:50 +0900

    2022/08/14

--- abc084/C/main.py
+++ abc084/C/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+import math
+N = int(input())
+
+vec = []
+for i in range(N - 1):
+  vec.append(list(map(int, input().split())))
+
+result = []
+for i in range(N):
+  t = 0
+  for j, (c, s, f) in enumerate(vec[i:]):
+    if s >= t:
+      t = s
+    else:
+      n = int((t - s - 0.0001) // f)
+      next_dep = s + (n + 1) * f
+      t = next_dep
+
+    t += c
+  result.append(t)
+
+for r in result:
+  print(r)
--- abc089/C/main.py
+++ abc089/C/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+from collections import Counter
+N = int(input())
+
+counter = Counter()
+for i in range(N):
+  data = input()
+  v = data[0]
+  if v in 'MARCH':
+    counter[v] += 1
+
+keys = 'MARCH'
+l = len(keys)
+total = 0
+for i in range(l - 2):
+  for j in range(i + 1, l - 1):
+    for k in range(j + 1, l):
+      comb = counter[keys[i]] * counter[keys[j]] * counter[keys[k]]
+      total += comb
+
+print(total)
