8dc38da882bbef883eaff389e9173531dc200e38
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-16 01:32:22 +0900

    2022/08/16

--- abc094/C/main.py
+++ abc094/C/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+N = int(input())
+X = list(map(int, input().split()))
+Y = [y for y in X]
+
+Y.sort()
+
+left = Y[:N//2]
+last_left = left[-1]
+right = Y[N//2:]
+first_right = right[0]
+
+left = set(left)
+right = set(right)
+
+for x in X:
+  if x in left:
+    print(first_right)
+  else:
+    print(last_left)
--- abc097/C/main.py
+++ abc097/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+s = input()
+k = int(input())
+
+ans = set()
+for l in range(1, 6):
+  for i in range(len(s) - l + 1):
+    c = s[i:i+l]
+    ans.add(c)
+
+ans = sorted(ans)
+print(ans[k - 1])
