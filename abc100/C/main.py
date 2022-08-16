70d3544ce080a7f06542d39b54c77c468a845e45
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-17 01:02:26 +0900

    2022/08/17

--- abc100/C/main.py
+++ abc100/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+N = int(input())
+a = list(map(int, input().split()))
+
+cnt = 0
+for x in a:
+  while x > 0:
+    if x % 2 == 0:
+      cnt += 1
+      x //= 2
+    else:
+      break
+
+print(cnt)
--- abc101/C/main.py
+++ abc101/C/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+if N == K:
+  print(1)
+  exit()
+
+one_pos = A.index(1)
+A = A[:one_pos] + A[one_pos + 1:]
+
+cnt = 0
+for i in range(0, N - 1, K - 1):
+  # print(A[i:i+K-1])
+  cnt += 1
+
+print(cnt)
