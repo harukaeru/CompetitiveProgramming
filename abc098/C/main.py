fbce9d54367a9f11c4a68aaf1e07380fc6bee3d8
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-16 21:48:27 +0900

    2022/08/16

--- abc098/C/main.py
+++ abc098/C/main.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+N = int(input())
+S = input()
+
+min_d = 3 * (10 ** 5) + 1
+w_counter = [0] * (N + 1)
+for i in range(N):
+  w_counter[i + 1] += w_counter[i]
+  if S[i] == 'W':
+    w_counter[i + 1] += 1
+
+e_counter = [0] * (N + 1)
+S2 = S[::-1]
+for i in range(N):
+  e_counter[i + 1] += e_counter[i]
+  if S2[i] == 'E':
+    e_counter[i + 1] += 1
+e_counter.reverse()
+
+# print(w_counter)
+# print(e_counter)
+for i in range(1, N + 1):
+  # a = S[:i].count('W')
+  a = w_counter[i - 1]
+  # b = S[i + 1:].count('E')
+  b = e_counter[i]
+  min_d = min(min_d, a + b)
+  
+print(min_d)
--- abc099/C/main.py
+++ abc099/C/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+N = int(input())
+
+candidates = [1] + [6 ** i for i in range(1, 7)] + [9 ** i for i in range(1, 6)]
+
+dp = [0] * (N + 1)
+dp[1] = 1
+
+for i in range(2, N + 1):
+  m = 9999999999
+  for candidate in candidates:
+    if i >= candidate:
+      m = min(m, dp[i - candidate] + 1)
+  dp[i] = m
+
+print(dp[N])
