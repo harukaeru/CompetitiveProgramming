5860565b3aaf05aed85220d64f043a93cae6ba01
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-16 22:02:19 +0900

    2022/08/16

--- abc253/B/main.py
+++ abc253/B/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+H, W = map(int, input().split())
+S = []
+for i in range(H):
+  S.append(input())
+
+c = []
+for i in range(H):
+  for j in range(W):
+    if S[i][j] == 'o':
+      c.append((i, j))
+
+print(abs(c[0][0] - c[1][0]) + abs(c[0][1] - c[1][1]))
---
+++ mydp/max_sum/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+  dp[i] = dp[i - 1] + max(0, a[i - 1])
+
+print(dp)
+print(dp[n])
