7144f89d94e1257b61c8726f44d3053105c573dc
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-19 02:35:45 +0900

    2022/08/19

--- abc104/C/main.py
+++ abc104/C/main.py
@@ -1,1 +1,53 @@
 #!/usr/bin/env python3
+D, G = map(int, input().split())
+scores = []
+for i in range(D):
+  p, c = map(int, input().split())
+  scores.append((p, c))
+
+
+all_selected = sum([s[0] + 1 for s in scores])
+dp = []
+
+for d in range(D + 1):
+  dp.append([0] * all_selected)
+
+min_count = 99999999999
+for i, (problem_count, complete) in enumerate(scores):
+
+  for j in range(len(dp[i]) - problem_count):
+    for count in range(problem_count + 1):
+      obtained_score = (i + 1) * 100 * count
+      if count == problem_count:
+        obtained_score += complete
+
+      dp[i + 1][j + count] = max(obtained_score + dp[i][j], dp[i][j + count], dp[i + 1][j+count])
+      # print(dp[i + 1])
+      if dp[i + 1][j + count] >= G:
+        min_count = min(min_count, j + count)
+
+print(min_count)
+
+# ps = [s[0] + 1 for s in scores]
+# 
+# m = 1
+# for p in ps:
+#   m *= p
+
+# ans = 999999999
+# print('m', m)
+# for i in range(m):
+#   tmp = i
+#   current_score = 0
+#   total_count = 0
+#   for score_i, p in enumerate(ps):
+#     count = tmp % p
+#     total_count += count
+#     current_score += (score_i + 1) * 100 * count
+#     if count == p - 1:
+#       bonus = scores[score_i][1]
+#       current_score += bonus
+#     tmp //= p
+# 
+#   if current_score >= G:
+#     ans = min(ans, total_count)
--- abc255/B/main.py
+++ abc255/B/main.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+import math
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+lights = set(A)
+
+points = []
+for i in range(N):
+  x, y = map(int, input().split())
+  points.append((x, y))
+
+closest_distance_list = []
+for i in range(N):
+  if i + 1 in lights:
+    continue
+
+  min_d_2 = 99999999999999
+  point = points[i]
+  for a in A:
+    light = points[a - 1]
+
+    dx = light[0] - point[0]
+    dy = light[1] - point[1]
+    d_2 = dx * dx + dy * dy
+    min_d_2 = min(min_d_2, d_2)
+  closest_distance_list.append(min_d_2)
+
+required_r_2 = max(closest_distance_list)
+
+print(math.sqrt(required_r_2))
