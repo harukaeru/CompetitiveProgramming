4af6ea6b8e16249d1b72cc5c727309b306831379
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-20 20:14:06 +0900

    2022/08/20

--- abc255/C/random_test.py
+++ abc255/C/random_test.py
@@ -3,12 +3,12 @@ import sys
 import os
 import random
 
-# config
-IN_START = 5
-IN_END = 60
-EXECUTION_COMMAND = './to_test.py'
-
 ##############################################################################
+
+# config
+IN_TXT_START = 5
+IN_TXT_END = 60
+EXECUTION_COMMAND = './to_test.py'
 
 input_template = '''
 {X} {A} {D} {N}
@@ -25,17 +25,22 @@ def create_context():
 ##############################################################################
 
 def generate_test():
-    for i in range(IN_START, IN_END + 1):
+    for i in range(IN_TXT_START, IN_TXT_END + 1):
         ctx = create_context()
-        input_string = input_template.format(**ctx).strip()
-        print(f'Generating in_{i}.txt & out_{i}.txt ...')
-        with open(f'in_{i}.txt', 'w') as f:
+        input_string = input_template.format(**ctx).strip() + '\n'
+
+        in_file = f'in_{i}.txt'
+        out_file = f'out_{i}.txt'
+        print('.', end='', flush=True)
+        with open(in_file, 'w') as f:
             f.write(input_string)
-        os.system(f'cat in_{i}.txt | {EXECUTION_COMMAND} > out_{i}.txt')
+        os.system(f'cat {in_file} | {EXECUTION_COMMAND} > {out_file}')
+    print()
 
 # usage:
-# $ python3 random_test.py test
+# $ python3 random_test.py
 
 generate_test()
+
 if len(sys.argv) > 1:
     os.system('atcoder-tools test')
