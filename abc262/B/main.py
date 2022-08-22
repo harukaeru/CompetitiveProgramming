b4223698b3562ef9e1a243723387ea98547977a3
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-22 23:41:27 +0900

    2022/08/22

--- abc262/B/main.py
+++ abc262/B/main.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+n, m = map(int, input().split())
+
+nodes = {}
+
+for i in range(m):
+  u, v = map(int, input().split())
+
+  nodes.setdefault(u, set()).add(v)
+  nodes.setdefault(v, set()).add(u)
+
+# print(nodes)
+
+cnt = 0
+for a in range(1, 99):
+  for b in range(a + 1, 100):
+    for c in range(b + 1, 101):
+      if nodes.get(a) and nodes.get(b) and nodes.get(c):
+        if b in nodes[a] and c in nodes[b] and a in nodes[c]:
+          cnt += 1
+
+print(cnt)
--- abc263/B/main.py
+++ abc263/B/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+N = int(input())
+P = list(map(int, input().split()))
+
+a = N
+cnt = 0
+while a != 1:
+  a = P[a - 2]
+  cnt += 1
+
+print(cnt)
