e3985742635f819a2e5cf5198debf8bdc30fce66
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-23 00:02:02 +0900

    2022/08/23

--- abc257/B/main.py
+++ abc257/B/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+N, K, Q = map(int, input().split())
+A = list(map(int, input().split()))
+L = list(map(int, input().split()))
+
+for i in range(Q):
+  pawn = A[L[i] - 1]
+  if pawn == N:
+    continue
+  try:
+    A.index(pawn + 1)
+    continue
+  except:
+    A[L[i] - 1] = pawn + 1
+
+print(' '.join(map(str, A)))
--- abc261/B/main.py
+++ abc261/B/main.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+N = int(input())
+
+A = []
+for i in range(N):
+  a = input()
+  A.append(a)
+
+for i in range(N):
+  for j in range(N):
+    # print('i,j', i,j )
+    if i == j:
+      continue
+    if A[i][j] == 'W':
+      if A[j][i] != 'L':
+        print('incorrect')
+        exit()
+    if A[i][j] == 'L':
+      if A[j][i] != 'W':
+        print('incorrect')
+        exit()
+    if A[i][j] == 'D':
+      if A[j][i] != 'D':
+        print('incorrect')
+        exit()
+print('correct')
