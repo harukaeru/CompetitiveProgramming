19674fb3a98ac98c632e421460b9c862612748db
harukaeru <nekoneko.myaomyao@gmail.com>
2022-08-26 01:52:38 +0900

    2022/08/26

--- abc111/C/main.py
+++ abc111/C/main.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+from collections import Counter
+n = int(input())
+a = list(map(int, input().split()))
+
+b = []
+c = []
+
+for i, x in enumerate(a):
+  if i % 2 == 0:
+    b.append(x)
+  else:
+    c.append(x)
+
+def sort_by_index1(b):
+  return -b[1]
+
+b_counter = list(sorted(Counter(b).items(), key=sort_by_index1))
+c_counter = list(sorted(Counter(c).items(), key=sort_by_index1))
+
+rest_b_counter = [(b[0], n // 2 - b[1]) for b in b_counter] + [(-1, n // 2)]
+rest_c_counter = [(c[0], n // 2 - c[1]) for c in c_counter] + [(-1, n // 2)]
+
+minimum_group = set([rest_c_counter[0][0]])
+minimum_rest = rest_c_counter[0][1]
+second_minimum_rest = None
+for rest_c in rest_c_counter[1:]:
+  if rest_c[1] == minimum_rest:
+    minimum_group.add(rest_c[0])
+  else:
+    second_minimum_rest = rest_c[1]
+    break
+
+total = 999999999999999999
+for bx in rest_b_counter:
+  b_value = bx[0]
+  b_count = bx[1]
+  c_count = second_minimum_rest if b_value in minimum_group and len(minimum_group) == 1 else minimum_rest
+  # print(b_value, '->', b_count, c_count)
+  total = min(total, b_count + c_count)
+
+print(total)
--- abc260/B/main.py
+++ abc260/B/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+N, X, Y, Z = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = []
+
+new_A = [(A[i], -i) for i in range(N)]
+new_A.sort(reverse=True)
+ans += [-a[1] for a in new_A[:X]]
+# print('After A', ans)
+
+new_B = [(B[i], -i) for i in range(N) if i not in ans]
+new_B.sort(reverse=True)
+ans += [-b[1] for b in new_B[:Y]]
+# print('After B', ans)
+
+new_AB = [(A[i] + B[i], -i) for i in range(N) if i not in ans]
+new_AB.sort(reverse=True)
+ans += [-ab[1] for ab in new_AB[:Z]]
+# print('After AB', ans)
+
+for a in sorted(ans):
+  print(a + 1)
