01ac5e8982b7b2223396294ae839a904e53c10e0
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-01 03:42:26 +0900

    2022/09/01

--- abc131/C/main.py
+++ abc131/C/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import math
+A, B, C, D = map(int, input().split())
+
+l = C * D // math.gcd(C, D)
+
+c_count = B // C - (A - 1) // C
+d_count = B // D - (A - 1) // D
+cd_count = B // l - (A - 1) // l
+
+# print('c_count', c_count)
+# print('d_count', d_count)
+# print('cd_count', cd_count)
+ng = c_count + d_count - cd_count
+
+print((B - A + 1) - ng)
--- abc224/C/main.py
+++ abc224/C/main.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+N = int(input())
+xy = []
+for i in range(N):
+  x, y = map(int, input().split())
+  xy.append((x, y))
+
+# xy.sort()
+cnt = 0
+total = 0
+for i in range(N - 2):
+  x_i, y_i = xy[i]
+  for j in range(i + 1, N - 1):
+    x_j, y_j = xy[j]
+    dx1 = x_j - x_i
+    dy1 = y_j - y_i
+
+    for k in range(j + 1, N):
+      total += 1
+      x_k, y_k = xy[k]
+
+      dx2 = x_k - x_i
+      dy2 = y_k - y_i
+
+      if dx1 * dy2 == dy1 * dx2:
+        cnt += 1
+
+print(total - cnt)
