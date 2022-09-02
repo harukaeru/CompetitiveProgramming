3370cef8922282a820b0c13c37f8468929cb950e
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-02 09:19:32 +0900

    2022/09/02

--- abc138/C/main.py
+++ abc138/C/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+N = int(input())
+v = [a for a in map(int, input().split())]
+
+v.sort()
+
+value = (v[1] + v[0]) / 2
+for i in range(2, N):
+  value = (value + v[i]) / 2
+
+print(value)
--- abc139/C/main.py
+++ abc139/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+N = int(input())
+H = list(map(int, input().split()))
+
+increaser = [0]
+for i in range(N - 1):
+  if H[i + 1] <= H[i]:
+    increaser.append(increaser[i] + 1)
+  else:
+    increaser.append(0)
+
+print(max(increaser))
