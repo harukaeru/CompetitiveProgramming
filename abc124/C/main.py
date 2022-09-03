0afe23ceb13541f33a52bddc727ca69f15a8e5c0
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-04 00:15:02 +0900

    2022/09/04

--- abc124/C/main.py
+++ abc124/C/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+S = input()
+
+current = '0'
+s_cnt = 0
+for i in range(len(S)):
+  new_current = '1' if current == '0' else '0'
+  if S[i] == current:
+    s_cnt += 1
+  current = new_current
+
+current = '1'
+t_cnt = 0
+for i in range(len(S)):
+  new_current = '1' if current == '0' else '0'
+  if S[i] == current:
+    t_cnt += 1
+  current = new_current
+
+
+print(min(s_cnt, t_cnt))
