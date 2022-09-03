a10f1e1ac401a99612bd1573ad4ff9e1b0af6fda
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-03 23:06:59 +0900

    2022/09/03

--- abc126/C/main.py
+++ abc126/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import math
+N, K = map(int, input().split())
+
+b = math.log(K, 2)
+t = 0
+for i in range(1, N + 1):
+  if i >= K:
+    t += 1 / N
+    continue
+
+  p = b - math.log(i, 2)
+  p = math.ceil(p)
+
+  t += 1 / N * ((1 / 2) ** p)
+
+  # print(i, p)
+print(t)
--- abc137/C/main.py
+++ abc137/C/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+import math
+from collections import Counter
+N = int(input())
+
+S = []
+for i in range(N):
+  s = list(input())
+  s.sort()
+  s = ''.join(s)
+  S.append(s)
+
+counter = Counter(S)
+# print(counter)
+
+total = 0
+for value in counter.values():
+  if value == 1:
+    continue
+
+  total += math.comb(value, 2)
+
+print(total)
