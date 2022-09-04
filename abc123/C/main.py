c91fdc2db6e418af7ddb54ed530f05eb0fb99901
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-04 19:38:23 +0900

    2022/09/04

--- abc123/C/main.py
+++ abc123/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+N = int(input())
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+
+m = min([A, B, C, D, E])
+
+cnt = (N + m - 1) // m
+# print('cnt', cnt)
+print(cnt - 1 + 5)
