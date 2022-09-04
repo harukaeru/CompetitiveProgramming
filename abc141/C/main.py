e159d400761f09e906137f71c60f006630a66e1d
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-04 21:43:03 +0900

    2022/09/04

--- abc141/C/main.py
+++ abc141/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+N, K, Q = map(int, input().split())
+
+counter = [0] * N
+for i in range(Q):
+  A = int(input())
+  counter[A - 1] += 1
+
+for i, c in enumerate(counter):
+  if K - (Q - c) > 0:
+    print('Yes')
+  else:
+    print('No')
--- abc142/C/main.py
+++ abc142/C/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+N = int(input())
+A = list(map(int, input().split()))
+
+revs = [None] * N
+for i in range(N):
+  id = A[i] - 1
+  revs[id] = i + 1
+
+print(' '.join(map(str, revs)))
