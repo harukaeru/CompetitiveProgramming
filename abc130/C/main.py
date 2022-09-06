bbeceb45adb74a1fc1df12f19a9634f2ea9f6e23
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-06 23:04:05 +0900

    2022/09/06

--- abc130/C/main.py
+++ abc130/C/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+W, H, x, y = map(int, input().split())
+
+g_0, g_1 = W / 2, H / 2
+
+S = W * H / 2
+
+if g_0 == x and g_1 == y:
+  print(S, 1)
+else:
+  print(S, 0)
--- abc153/C/main.py
+++ abc153/C/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+N, K = map(int, input().split())
+H = list(map(int, input().split()))
+
+H.sort()
+H.reverse()
+
+H = H[K:]
+
+print(sum(H))
