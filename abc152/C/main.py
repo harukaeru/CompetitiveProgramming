442cc62f198ff73785b5c9f52c136d0229c24f61
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-06 23:12:52 +0900

    2022/09/06

--- abc152/C/main.py
+++ abc152/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+N = int(input())
+P = list(map(int, input().split()))
+
+current_min = 999999999999
+
+cnt = 0
+for i in range(N):
+  if P[i] <= current_min:
+    cnt += 1
+  current_min = min(current_min, P[i])
+
+print(cnt)
