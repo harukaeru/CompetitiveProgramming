dd645b3f8f360b7f9070f79b9c980278dc6ff67d
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-07 23:58:23 +0900

    2022/09/07

--- abc168/C/main.py
+++ abc168/C/main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import math
+A, B, H, M = map(int, input().split())
+
+h = 360 * H / 12 + 30 * M / 60
+m = 360 * M / 60
+
+deg = abs(h - m)
+
+if deg == 0:
+  print(abs(A - B))
+elif deg == 180:
+  print(abs(A + B))
+else:
+  print(math.sqrt(A * A + B * B - 2 * A * B * math.cos(math.radians(deg))))
--- abc179/C/main.py
+++ abc179/C/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+import math
+N = int(input())
+
+cnt = 0
+for c in range(1, N + 1):
+  ab = N - c
+  math.sqrt(ab)
+  
+
+print(cnt)
