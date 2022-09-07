5aa5d64083d8e4ac70e427c1f98935fb49466c7c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-08 00:10:36 +0900

    2022/09/08

--- abc179/C/main.py
+++ abc179/C/main.py
@@ -3,9 +3,14 @@ import math
 N = int(input())
 
 cnt = 0
-for c in range(1, N + 1):
+for c in range(1, N):
   ab = N - c
-  math.sqrt(ab)
-  
+  s = math.sqrt(ab)
+  for a in range(1, int(s) + 1):
+    if ab % a == 0:
+      cnt += 2
+
+  if s == int(s):
+    cnt -= 1
 
 print(cnt)
