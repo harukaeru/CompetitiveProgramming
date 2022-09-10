5c8dbcd287ee1df266f6a79f1e7433aff604d37a
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-11 01:00:45 +0900

    2022/09/11

--- abc166/C/main.py
+++ abc166/C/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+N, M = map(int, input().split())
+H = list(map(int, input().split()))
+
+paths = {
+
+} 
+for i in range(M):
+  a, b = map(int, input().split())
+  a, b = a - 1, b - 1
+  paths.setdefault(a, []).append(b)
+  paths.setdefault(b, []).append(a)
+
+cnt = 0
+for i in range(N):
+  greatest = True
+  for p in paths.get(i, []):
+    if H[i] <= H[p]:
+      greatest = False
+      break
+  if greatest:
+    cnt += 1
+
+print(cnt)
