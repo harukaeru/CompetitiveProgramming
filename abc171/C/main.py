bd15b05d1c38bb4fa68ba790f2484d3ea9801de8
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-12 23:43:34 +0900

    2022/09/12

--- abc171/C/main.py
+++ abc171/C/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+N = int(input())
+
+d = -1
+for i in range(1, 12):
+  k = 26 ** i
+  if N <= k:
+    d = i
+    break
+  N -= k
+
+ans = ''
+N -= 1
+for i in range(d):
+  k = N % 26
+  ans = chr(ord('a') + k) + ans
+  N //= 26
+
+print(ans)
--- abc182/C/main.py
+++ abc182/C/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+from itertools import combinations
+
+N = input()
+N = list(N)
+
+min_ok = 99999
+for i in range(len(N)):
+  ch = len(N) - i
+  comb = combinations(N, ch)
+  for l in comb:
+    s = ''.join(l)
+    if int(s) % 3 == 0:
+      min_ok = min(min_ok, i)
+
+if min_ok == 99999:
+  print(-1)
+else:
+  print(min_ok)
