9baf1aa0b9a29f1ea75596cd09432b4cb78ba433
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-13 02:19:07 +0900

    2022/09/13

--- abc252/C/main.py
+++ abc252/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+N = int(input())
+S = []
+for i in range(N):
+  S.append(input())
+
+min_tot = 999999
+for t in range(10):
+  tot = 0
+  positions = [0] * 10
+  for s in S:
+    pos = s.index(str(t))
+    val = 10 * positions[pos] + pos
+    tot = max(val, tot)
+    positions[pos] += 1
+  min_tot = min(min_tot, tot)
+
+print(min_tot)
