f10e8edd06659040fca79b6dc3dcf3b92afa41a7
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-13 01:58:43 +0900

    2022/09/13

--- abc183/C/main.py
+++ abc183/C/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+from itertools import permutations
+N, K = map(int, input().split())
+T = []
+for i in range(N):
+  t = list(map(int, input().split()))
+  T.append(t)
+
+mids = list(range(1, N))
+
+cnt = 0
+for path in permutations(mids):
+  current = 0
+  time = 0
+  for i, dest in enumerate(path):
+    time += T[current][dest]
+    current = dest
+  time += T[current][0]
+
+  if time == K:
+    cnt += 1
+
+print(cnt)
+  
