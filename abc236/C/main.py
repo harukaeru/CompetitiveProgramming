2c872590f74615dd3d461c845838503f9dae00be
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-16 02:46:45 +0900

    2022/09/16

--- abc236/C/main.py
+++ abc236/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+N, M = map(int, input().split())
+S = input().split()
+T = input().split()
+
+T = set(T)
+
+for s in S:
+  if s in T:
+    print('Yes')
+  else:
+    print('No')
