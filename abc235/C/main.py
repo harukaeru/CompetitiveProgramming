2ff8b523ef701d6a30d17e2be50d071f2ef6b3a4
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-17 16:42:08 +0900

    2022/09/17

--- abc235/C/main.py
+++ abc235/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+counter = defaultdict(int)
+qs = {}
+for i in range(N):
+  a = A[i]
+  counter[a] += 1
+  qs[(a, counter[a])] = i
+
+for i in range(Q):
+  x, k = map(int, input().split())
+  if qs.get((x, k)) is not None:
+    print(qs.get((x, k)) + 1)
+  else:
+    print(-1)
