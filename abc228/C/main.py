a0006bc14f97f4c23195972ba7290346c036cd5d
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-19 13:05:17 +0900

    2022/09/19

--- abc228/C/main.py
+++ abc228/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import bisect
+N, K = map(int, input().split())
+scores = []
+for i in range(N):
+  P = list(map(int, input().split()))
+  scores.append(sum(P))
+
+thirds = list(sorted(scores))
+for s in scores:
+  fourth = s + 300
+  low_rank = bisect.bisect_right(thirds, fourth)
+  rank = len(scores) + 1 - low_rank
+  if rank <= K:
+    print('Yes')
+  else:
+    print('No')
+  # print(rank)
--- abc229/C/main.py
+++ abc229/C/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+N, W = map(int, input().split())
+
+AB = []
+for i in range(N):
+  a, b = map(int, input().split())
+  AB.append((a, b))
+
+AB.sort()
+AB.reverse()
+d = 0
+for a, b in AB:
+  chosen_cnt = min(W, b)
+  W -= chosen_cnt
+  d += a * chosen_cnt
+
+print(d)
