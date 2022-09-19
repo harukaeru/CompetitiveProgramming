a13088c84a0b45e59ff60b0b98f6166e628886d7
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-19 15:45:47 +0900

    2022/09/19

--- abc191/C/main.py
+++ abc191/C/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+H, W = map(int, input().split())
+S = []
+for i in range(H):
+  S.append(input())
+
+v = 0
+for x in range(1, H):
+  for y in range(1, W):
+    b = int(S[x - 1][y - 1] == '#')
+    b += int(S[x - 1][y] == '#')
+    b += int(S[x][y - 1] == '#')
+    b += int(S[x][y] == '#')
+    if b == 1 or b == 3:
+      v += 1
+
+print(v)
--- abc230/C/main.py
+++ abc230/C/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+N, A, B = map(int, input().split())
+P, Q, R, S = map(int, input().split())
+
+ans = []
+for i in range(P, Q + 1):
+  row = ['.'] * (S - R + 1)
+  ans.append(row)
+
+for x in range(P, Q + 1):
+  for y in range(R, S + 1):
+    vec = (x - A, y - B)
+    if vec[0] != vec[1] and vec[0] != -vec[1]:
+      continue
+
+    ans[x - P][y - R] = '#'
+
+for a in ans:
+  print(''.join(a))
