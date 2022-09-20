177080c1c711d322c1d84d7a72024696990ee2b5
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-21 01:56:41 +0900

    2022/09/21

--- abc232/C/main.py
+++ abc232/C/main.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+from itertools import permutations
+from collections import defaultdict
+
+def get_graph(seeds):
+  B = defaultdict(list)
+  for x, y in seeds:
+    B[x].append(y)
+    B[y].append(x)
+  for value in B.values():
+    value.sort()
+  return B
+
+N, M = map(int, input().split())
+
+first = []
+for i in range(M):
+  x, y = map(int, input().split())
+  first.append((x, y))
+
+second = []
+for i in range(M):
+  x, y = map(int, input().split())
+  second.append((x, y))
+
+A = get_graph(first)
+
+seeds = list(range(1, N + 1))
+for converter in permutations(seeds):
+  second2 = []
+  for x, y in second:
+    x2 = converter[x - 1]
+    y2 = converter[y - 1]
+    second2.append((x2, y2))
+  B = get_graph(second2)
+  if A == B:
+    print('Yes')
+    exit()
+
+print('No')
--- abc233/C/main.py
+++ abc233/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+N, X = map(int, input().split())
+L = []
+A = []
+for i in range(N):
+  l, *a = map(int, input().split())
+  L.append(l)
+  A.append(a)
+
+dp = [defaultdict(int) for i in range(N + 1)]
+dp[0][1] = 1
+for i in range(N):
+  for a in A[i]:
+    for prev, count in dp[i].items():
+      dp[i + 1][prev * a] += count
+  
+print(dp[N][X])
