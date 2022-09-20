ec5c6d951e276f01576f1e2592b9af573c088a03
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-21 02:26:57 +0900

    2022/09/21

--- abc238/C/main.py
+++ abc238/C/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+MOD = 998244353
+N = int(input())
+
+d = len(str(N))
+cnt = 0
+for i in range(d):
+  k = i + 1
+  base = 10 ** (k - 1)
+  if i == d - 1:
+    m = N
+  else:
+    m = 10 ** k - 1
+
+  base_cnt = 1
+  m_cnt = m - base + 1
+
+  c = (m_cnt - base_cnt + 1) * (base_cnt + m_cnt) // 2
+  cnt += c % MOD
+
+print(cnt % MOD)
--- abc239/C/main.py
+++ abc239/C/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+x1, y1, x2, y2 = map(int, input().split())
+
+ds = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
+
+for d1 in ds:
+  for d2 in ds:
+    if x2 + d2[0] == x1 + d1[0] and y2 + d2[1] == y1 + d1[1]:
+      print('Yes')
+      exit()
+print('No')
