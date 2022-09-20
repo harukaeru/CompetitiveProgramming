ce4db83e0496cddc04323c675c15f00c302c10a4
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-20 22:51:41 +0900

    2022/09/20

--- abc231/C/main.py
+++ abc231/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+import bisect
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+A.sort()
+def get_ans(x):
+  pos = bisect.bisect_left(A, x)
+  return len(A) - pos
+
+for i in range(Q):
+  x = int(input())
+  print(get_ans(x))
--- abc234/C/main.py
+++ abc234/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+K = int(input())
+
+s = ''
+while K > 0:
+  if K % 2:
+    s = '2' + s
+  else:
+    s = '0' + s
+  K //= 2
+
+print(s)
