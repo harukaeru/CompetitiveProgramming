542c472107d1c2851b308b23bfdd0eb8923e1e40
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-24 05:32:32 +0900

    2022/09/24

---
+++ utils/pig.py
@@ -0,0 +1,13 @@
+child_dp = [0] * 241
+adult_dp = [0] * 241
+adult_dp[0] = 1
+
+for i in range(241):
+  if i + 5 <= 240:
+    child_dp[i + 5] += adult_dp[i] * 10
+    adult_dp[i + 5] += child_dp[i] + adult_dp[i]
+
+print(child_dp)
+print(adult_dp)
+
+print('ans->', adult_dp[240])
