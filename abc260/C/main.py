a66e60a6c3dcef31b6e60cccaa6497f4c69a9cbf
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-23 23:01:36 +0900

    2022/09/23

--- abc260/C/main.py
+++ abc260/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+N, X, Y = map(int, input().split())
+reds = [0] * (N + 1)
+blues = [0] * (N + 1)
+reds[N] = 1
+
+for n in range(N, 1, -1):
+  reds[n - 1] += reds[n]
+  blues[n] += reds[n] * X
+
+  blues[n - 1] = blues[n] * Y
+  reds[n - 1] += blues[n]
+
+print(blues[1])
