eac57151ba7c1427d1c5a76c48b26f61888842cf
harukaeru <nekoneko.myaomyao@gmail.com>
2022-09-24 16:03:48 +0900

    2022/09/24

--- abc263/C/main.py
+++ abc263/C/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+N, M = map(int, input().split())
+
+A = []
+
+def dfs(s):
+  if len(A) == N:
+    print(' '.join(map(str, A)))
+    return
+
+  for j in range(s + 1, M + 1):
+    A.append(j)
+    dfs(j)
+    A.pop()
+
+dfs(0)
