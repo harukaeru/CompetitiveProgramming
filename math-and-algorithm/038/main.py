42d3b67eeee2abc1d1a4f08476bb9618671ef4e0
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-05 01:33:39 +0900

    2022/10/05

--- math-and-algorithm/038/main.py
+++ math-and-algorithm/038/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = [0] * (N + 1)
+for i in range(1, N + 1):
+  dp[i] = dp[i - 1] + A[i - 1]
+
+for i in range(Q):
+  l, r = map(int, input().split())
+  print(dp[r] - dp[l - 1])
