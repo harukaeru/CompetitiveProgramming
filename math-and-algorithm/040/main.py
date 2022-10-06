77950f070c444a0bb7ca92983b6cd71b8ee43338
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-06 23:09:41 +0900

    2022/10/06

--- math-and-algorithm/040/main.py
+++ math-and-algorithm/040/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = []
+for i in range(M):
+  B.append(int(input()))
+
+d = [0] * N
+for i in range(1, N):
+  d[i] = d[i - 1] + A[i - 1]
+
+before = B[0]
+s = 0
+for current in B[1:]:
+  s += d[max(current, before) - 1] - d[min(current, before) - 1]
+  before = current
+
+print(s)
+  
