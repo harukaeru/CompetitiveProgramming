ac28f3b2db6c7efa9004e5f1fd0fb67663b63cf1
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-07 16:57:14 +0900

    2022/10/07

--- math-and-algorithm/041/main.py
+++ math-and-algorithm/041/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+T = int(input())
+N = int(input())
+
+P = [0] * (T + 1)
+for i in range(N):
+  l, r = map(int, input().split())
+  P[l] += 1
+  P[r] -= 1
+
+sp = [0] * (T + 2)
+for i, p in enumerate(P):
+  sp[i + 1] = sp[i] + p
+
+for s in sp[1:T + 1]:
+  print(s)
