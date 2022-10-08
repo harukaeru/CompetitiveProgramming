11c7938f37c578f339e852b744c575bbb5fdeeef
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-09 02:26:48 +0900

    2022/10/09

--- abc270/B/main.py
+++ abc270/B/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+X, Y, Z = map(int, input().split())
+
+
+if X < 0:
+  X = -X
+  Y = -Y
+  Z = -Z
+
+if X < Y:
+  print(X)
+elif Y < 0:
+  print(X)
+else:
+  if Y < Z:
+    print(-1)
+  elif 0 < Z:
+    print(X)
+  else:
+    print((-Z) * 2 + X)
