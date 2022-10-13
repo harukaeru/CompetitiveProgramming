c7ad36c191203a931505d5de765e971512e5fe6b
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-13 23:29:44 +0900

    2022/10/13

--- abc264/D/main.py
+++ abc264/D/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+S = list(input())
+
+def count(S, x):
+  c = S.index(x)
+  S.remove(x)
+  return c
+
+t = 0
+for s in 'atcoder':
+  t += count(S, s)
+
+print(t)
--- utils/444.py
+++ utils/444.py
@@ -2,8 +2,7 @@ from decimal import Decimal
 p = 1
 s = 0
 while s <= 30:
-  s += Decimal(1) / Decimal(p)
+  s += 1.0 / p
   p += 1
-  print('s', s)
 
 print(s)
