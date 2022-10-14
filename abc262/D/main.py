22ff10911ff512544bd173c4b9e29f9e72853a49
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-15 02:08:20 +0900

    2022/10/15

--- abc262/D/main.py
+++ abc262/D/main.py
@@ -1,1 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
+
+MOD = 998244353
+N = int(input())
+A = list(map(int, input().split()))
+
+def search(n, cnt):
+  dp = []
+  for i in range(N + 1):
+    dd = []
+    for j in range(cnt + 1):
+      d = [0] * (n)
+      dd.append(d)
+    dp.append(dd)
+  dp[0][0][0] = 1
+  # print(dp)
+
+  # i個目で、counter個すでにえらんでいて、j mod nになる
+  for i in range(1, N + 1):
+    chosen = A[i - 1]
+    for counter in range(1, cnt + 1):
+      for j in range(n):
+        dp[i][counter][(j + chosen) % n] += dp[i - 1][counter - 1][j]
+        dp[i][counter - 1][(j + chosen) % n] += dp[i - 1][counter - 1][(j + chosen) % n]
+  # for dd in dp:
+  #   print('-----')
+  #   for d in dd:
+  #     print(d)
+  return dp
+
+tot = 0
+for i in range(1, N + 1):
+  tot += search(i, N)[N][i][0] % MOD
+print(tot % MOD)
--- abc263/D/main.py
+++ abc263/D/main.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+N, K, R = map(int, input().split())
+A = list(map(int, input().split()))
+
+D = [0] * (N + 1)
+for i in range(1, N + 1):
+  D[i] = D[i - 1] + A[i - 1]
+
+E = [0] * (N + 1)
+for i in range(N, 0, -1):
+  E[i - 1] = E[i] + A[i - 1]
+
+for i in range(1, N + 1):
+  D[i] -= i * K
+
+for i in range(N, 0, -1):
+  E[i - 1] -= (N - i + 1) * R
+
+for i in range(1, N + 1):
+  D[i] = max(D[i - 1], D[i])
+
+for i in range(N, 0, -1):
+  E[i - 1] = max(E[i], E[i - 1])
+
+m = 0
+for i in range(0, N + 1):
+  m = max(m, D[i] + E[i])
+
+print(sum(A) - m)
