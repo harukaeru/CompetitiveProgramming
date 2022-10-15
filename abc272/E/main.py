f4bc8f5dfb7894ed154dc204e9031ed9313dbe4d
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-15 19:24:22 +0900

    2022/10/15

--- abc272/E/main.py
+++ abc272/E/main.py
@@ -1,1 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+memo=[set() for _ in range(N + 1)]
+ 
+for i, a in enumerate(A, 1):
+  cnt = max(0, -(a // i))
+  while a + i * cnt <= N:
+    memo[a + i * cnt].add(cnt)
+    cnt+=1
+ 
+for i in range(1, M + 1):
+  ans = 0
+  while i in memo[ans]:
+    ans+=1
+  print(ans)
