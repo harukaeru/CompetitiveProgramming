aa1c55225839675c0716b869a76455de5d22b0d5
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-19 03:47:25 +0900

    2022/10/19

--- math-and-algorithm/043/main.py
+++ math-and-algorithm/043/main.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+N, M = map(int, input().split())
+G = defaultdict(list)
+
+for i in range(M):
+  a, b = map(int, input().split())
+  G[a].append(b)
+  G[b].append(a)
+
+seen = set()
+q = []
+q.append(1)
+seen.add(1)
+
+while len(q) > 0:
+  v = q.pop()
+  for nex in G[v]:
+    if nex not in seen:
+      seen.add(nex)
+      q.append(nex)
+  
+if len(seen) != N:
+  print('The graph is not connected.')
+else:
+  print('The graph is connected.')
--- math-and-algorithm/044/main.py
+++ math-and-algorithm/044/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+from collections import defaultdict, deque
+N, M = map(int, input().split())
+G = defaultdict(list)
+for i in range(M):
+  a, b = map(int, input().split())
+  G[a].append(b)
+  G[b].append(a)
+
+q = deque()
+q.append(1)
+dists = [-1] * N
+dists[0] = 0
+
+while len(q) > 0:
+  v = q.popleft()
+  for nex in G[v]:
+    if dists[nex - 1] == -1:
+      dists[nex - 1] = dists[v - 1] + 1
+      q.append(nex)
+
+for i in range(N):
+  print(dists[i])
