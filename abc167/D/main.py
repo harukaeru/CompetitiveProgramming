3974b3492e7e963a525b956acacce3238a3322bb
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-21 20:20:06 +0900

    2022/10/21 ダブリングでも解いてみる

--- abc167/D/main.py
+++ abc167/D/main.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
+import math
 N, K = map(int, input().split())
-A = list(map(int, input().split()))
+A = list([a - 1 for a in map(int, input().split())])
 
-pos = {}
-dests = []
-det_pos = -1
-next_city = 1
+nex = 1
 
-for i in range(N):
-  if pos.get(next_city) is not None:
-    det_pos = pos[next_city]
-    break
+d = math.ceil(math.log(K, 2))
 
-  dests.append(next_city)
-  pos[next_city] = i
-  next_city = A[next_city - 1]
 
-if det_pos >= K:
-  print(dests[K])
-  exit()
+dp = []
+for i in range(d + 1):
+  dd = [-1] * N
+  dp.append(dd)
 
-m = (K - det_pos) % (len(dests) - det_pos)
-print(dests[det_pos + m])
+dp[0] = A
+
+for i in range(d):
+  for v in range(N):
+    dp[i + 1][v] = dp[i][dp[i][v]]
+
+v = 0
+for i in range(d + 1):
+  if (K & (1 << i)) != 0:
+    # print('i', i)
+    # print('here', v)
+    # print(dp[i])
+    v = dp[i][v]
+
+print(v + 1)
