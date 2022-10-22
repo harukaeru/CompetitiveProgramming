6946e64291b265ab7527a9a5708fbf6504498159
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-22 20:58:59 +0900

    2022/10/22

--- math-and-algorithm/068/main.py
+++ math-and-algorithm/068/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import math
+N, K = map(int, input().split())
+V = list(map(int, input().split()))
+
+s = 0
+for i in range(K):
+  lcm = 1
+  cnt = 0
+  for v in V[:i]:
+    lcm = lcm * V[i] // math.gcd(lcm, V[i])
+  if i % 2 == 0:
+    s += N // lcm
+  else:
+    s -= N // lcm
+
+print(s)
