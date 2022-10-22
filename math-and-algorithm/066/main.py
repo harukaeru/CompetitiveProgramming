585438155155158ff8a1e4c467e5a4eea4e86cfb
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-22 18:20:10 +0900

    2022/10/22 余事象

--- math-and-algorithm/066/main.py
+++ math-and-algorithm/066/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+N, K = map(int, input().split())
+
+cnt = 0
+for b in range(1, N + 1):
+  for w in range(max(1, b - (K - 1)), min(b + K - 1, N) + 1):
+    for g in range(max(1, b - (K - 1)), min(b + K - 1, N) + 1):
+      if abs(w - g) < K:
+        cnt += 1
+
+print(N ** 3 - cnt)
