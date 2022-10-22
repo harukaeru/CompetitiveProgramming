5946034d34f99dc411c2e0a851f274f3a0f46454
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-22 17:36:34 +0900

    2022/10/22

--- math-and-algorithm/064/main.py
+++ math-and-algorithm/064/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+count = sum(A)
+if K < count:
+  print('No')
+  exit()
+
+if (K - count) % 2 == 0:
+  print('Yes')
+else:
+  print('No')
---
+++ panasonic2020/B/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+H, W = map(int, input().split())
+
+if H == 1 or W == 1:
+  print(1)
+else:
+  print((H * W + 1) // 2)
