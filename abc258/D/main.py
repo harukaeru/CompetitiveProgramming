459ecf1214668c10e678f792af0a1ede78f08b9a
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-23 18:58:39 +0900

    最小回数

--- abc258/D/main.py
+++ abc258/D/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+N, X = map(int, input().split())
+AB = []
+for i in range(N):
+  a, b = map(int, input().split())
+  AB.append((a, b))
+
+S = []
+s = 0
+for i in range(N):
+  s += AB[i][0] + AB[i][1]
+  S.append(s)
+
+m = 9999999999999999999999999999
+for i in range(N):
+  c = i + 1
+  if X <= c:
+    m = min(m, S[i])
+  else:
+    m = min(m, S[i] + (X - c) * AB[i][1])
+
+# print(S)
+print(m)
