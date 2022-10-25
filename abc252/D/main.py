8e7858915dd0cfcacf4865a041615d9f4e27c51c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-25 22:57:34 +0900

    bisect

--- abc252/D/main.py
+++ abc252/D/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+from collections import Counter
+import bisect
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+counter = Counter(A)
+S = list(sorted(set(A)))
+
+m = 0
+for j in range(len(S)):
+  l_cnt = bisect.bisect_left(A, S[j])
+  r_cnt = N - bisect.bisect_right(A, S[j])
+  m += l_cnt * r_cnt * counter[S[j]]
+
+print(m)
