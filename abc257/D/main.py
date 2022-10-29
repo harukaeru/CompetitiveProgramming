618be434cc2532687fdbbafb467247a296c4cc82
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-29 19:37:42 +0900

    外積

--- abc257/D/main.py
+++ abc257/D/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+import math
+from numpy import subtract, abs, minimum, maximum
+N = int(input())
+X = []
+Y = []
+P = []
+for i in range(N):
+  x, y, p = map(int, input().split())
+  X.append(x)
+  Y.append(y)
+  P.append(p)
+
+f = lambda x: abs(subtract.outer(x, x))
+d = (f(X) + f(Y)) / P
+for k in range(N):
+  cross = maximum.outer(d[:, k], d[k])
+  d = minimum(d, cross)
+
+ma = d.max(axis=0)
+print(math.ceil(ma.min()))
