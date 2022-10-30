81ec68e9dc8d5f2e211a1c9105cf90dbe9c10fd0
harukaeru <nekoneko.myaomyao@gmail.com>
2022-10-31 02:00:36 +0900

    Stack

--- abc240/D/main.py
+++ abc240/D/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+N = int(input())
+A = list(map(int, input().split()))
+
+S = []
+tot = 0
+
+for i in range(N):
+  if len(S) == 0:
+    tot += 1
+    S.append([A[i], 1])
+  elif S[-1][0] != A[i]:
+    tot += 1
+    S.append([A[i], 1])
+  else:
+    tot += 1
+    S[-1][1] += 1
+    if S[-1][1] == A[i]:
+      tot -= A[i]
+      S.pop()
+  print(tot)
