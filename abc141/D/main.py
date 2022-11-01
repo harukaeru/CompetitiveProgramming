c1baa88453d37994a59833643424339de531e12c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-02 00:52:40 +0900

    heapq

--- abc141/D/main.py
+++ abc141/D/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import heapq
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+A = [-a for a in A]
+heapq.heapify(A)
+
+for i in range(M):
+  a = heapq.heappop(A)
+  # print('a', -a)
+  b = -a // 2
+  heapq.heappush(A, -b)
+
+
+print(-sum(A))
