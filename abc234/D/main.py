43d03b6415867e7afd294491944316f3ed8eee17
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-01 23:14:49 +0900

    heapq

--- abc234/D/main.py
+++ abc234/D/main.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
+import heapq
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+
+Q = P[:K]
+
+heapq.heapify(Q)
+print(min(Q))
+
+for i in range(K, N):
+  minimax = heapq.heappop(Q)
+  minimax = max(minimax, P[i])
+  heapq.heappush(Q, minimax)
+
+  ans = heapq.heappop(Q)
+  print(ans)
+  heapq.heappush(Q, ans)
