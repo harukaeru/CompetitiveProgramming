b56712a79dd68f585ba475c698626433c6011832
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-03 14:51:11 +0900

    完全数

--- arc026/B/main.py
+++ arc026/B/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+import math
+N = int(input())
+
+if N == 1:
+  print('Deficient')
+  exit()
+
+m = int(math.sqrt(N)) + 1
+k = set([1])
+for a in range(2, m):
+  if N % a == 0:
+    b = N // a
+    k.add(a)
+    k.add(b)
+
+# print('k', k)
+k = sum(k)
+if k == N:
+  print('Perfect')
+elif k > N:
+  print('Abundant')
+else:
+  print('Deficient')
