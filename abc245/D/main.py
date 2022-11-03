4acf098f1c40113935eb174a5258ccac131dac84
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-03 22:17:49 +0900

    多項式の割り算

--- abc245/D/main.py
+++ abc245/D/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+A.reverse()
+C.reverse()
+
+B = []
+# print('CI', C)
+for i in range(M + 1):
+  b = C[i] // A[0]
+  B.append(b)
+  for j in range(N + 1):
+    C[i + j] -= A[j] * b
+  # print(C)
+# print('B', B)
+B.reverse()
+print(*B)
