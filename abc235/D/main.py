7301879cc6846e46e31380a0cb9f2e6d2167e645
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-04 01:47:05 +0900

    ABC235 D 別解

--- abc235/D/main.py
+++ abc235/D/main.py
@@ -1,1 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
+import math
+a, N = map(int, input().split())
+
+rest = 1000
+
+dp = []
+for i in range(rest + 1):
+  dp.append(set())
+
+M = 10 ** (len(str(N)) + 1)
+
+dp[0] = [1]
+
+checked = set()
+
+for i in range(1, rest + 1):
+  for d in dp[i - 1]:
+    k = d * a
+    if k < M and k not in checked:
+      dp[i].add(k)
+    if d >= 10 and d % 10 != 0:
+      n = int(str(d % 10) + str(d // 10))
+      if n < M and n not in checked:
+        dp[i].add(n)
+  # print('dp', dp[i])
+  if N in dp[i]:
+    print(i)
+    exit()
+  checked.update(dp[i])
+
+print(-1)
