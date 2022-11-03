ec7edac98382aa8d079742f9e526742470f02a69
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-03 14:31:40 +0900

    調和級数

--- abc249/D/main.py
+++ abc249/D/main.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+import math
+N = int(input())
+A = list(map(int, input().split()))
+
+m = max(A)
+P = [0] * (m + 1)
+
+for i in range(N):
+  P[A[i]] += 1
+# 
+# print(list(range(max(A) + 1)))
+# print(P)
+
+tot = 0
+for j in range(1, m + 1):
+  c = 0
+  for i in range(j, m + 1, j):
+    cnt = P[i]
+    if cnt == 0:
+      continue
+
+    k = i // j
+    p = cnt * P[j] * P[k]
+    c += p
+
+  tot += c
+
+print(tot)
---
+++ abc249/D/xgen.py
@@ -0,0 +1,3 @@
+N = 2 * (10 ** 5)
+print(N)
+print(*[10 ** 5 + k for k in range(N)])
