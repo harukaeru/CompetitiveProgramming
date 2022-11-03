e3b62d3efbf4cd11491eb9ec53b37cc34da6837a
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-03 17:36:48 +0900

    逆算＋区間和

--- abc248/D/main.py
+++ abc248/D/main.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+import bisect
+from collections import defaultdict
+N = int(input())
+A = list(map(int, input().split()))
+Q = int(input())
+
+
+xlist = defaultdict(list)
+for i in range(N):
+  xlist[A[i]].append(i + 1)
+
+for v in xlist.values():
+  v.sort()
+
+# print(xlist)
+
+for q in range(Q):
+  L, R, X = map(int, input().split())
+  l = bisect.bisect_left(xlist[X], L)
+  r = bisect.bisect_right(xlist[X], R)
+  print(r - l)
---
+++ abc248/D/xgen.py
@@ -0,0 +1,7 @@
+N = 2 * (10 ** 4)
+print(N)
+print(*list(range(1, N + 1)))
+Q = N
+print(Q)
+for i in range(Q):
+    print(1, Q, i + 1)
