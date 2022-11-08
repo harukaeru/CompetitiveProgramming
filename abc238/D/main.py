096c2070f447a7bcac4faf424a4f804c30c9c34c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-09 01:18:58 +0900

    ビット演算

--- abc238/D/main.py
+++ abc238/D/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+T = int(input())
+A = []
+S = []
+for i in range(T):
+  a, s = map(int, input().split())
+  A.append(a)
+  S.append(s)
+
+for i in range(T):
+  a = A[i]
+  s = S[i]
+  if a <= s and (a & (s - a)) == a:
+    print('Yes')
+  else:
+    print('No')
