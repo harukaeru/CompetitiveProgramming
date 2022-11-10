1b5441bbcaecbd20133998be3e3242510dce55ba
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-11 01:36:15 +0900

    累積和+絶対値 / 固定

--- abc186/D/main.py
+++ abc186/D/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+
+LS = [0] * (N + 1)
+for i in range(1, N + 1):
+  LS[i] = LS[i - 1] + A[i - 1]
+
+s = 0
+for i in range(1, N + 1):
+  right_sum = LS[N] - LS[i]
+  right = A[i - 1] * (N - i)
+  x = right_sum - right
+
+  s += x
+print(s)
--- abc246/D/main.py
+++ abc246/D/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+N = int(input())
+
+L = int((N ** (1/3) + 1) ** 3)
+
+def f(a, b):
+  p = a ** 2
+  q = b ** 2
+  return p * a + p * b + a * q + b * q
+
+X = 9999999999999999999
+j = 1000000
+for i in range(0, 1000001):
+  while f(i, j) >= N and j >= 0:
+    print(f'f({i},{j})', f(i,j))
+    X = min(X, f(i, j))
+    j -= 1
+
+print(X)
