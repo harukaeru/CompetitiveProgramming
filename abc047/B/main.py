e6b347d5b528cee7c6b287e601c5ea179b7e248b
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-12 15:00:55 +0900

    B埋め3

--- abc047/B/main.py
+++ abc047/B/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+W, H, N = map(int, input().split())
+
+rightmost = W
+leftmost = 0
+top = H
+bottom = 0
+for i in range(N):
+  x, y, a = map(int, input().split())
+  if a == 1:
+    leftmost = max(leftmost, x)
+  elif a == 2:
+    rightmost = min(rightmost, x)
+  elif a == 3:
+    bottom = max(bottom, y)
+  else:
+    top = min(top, y)
+
+print(max(rightmost - leftmost, 0) * max(top - bottom, 0))
