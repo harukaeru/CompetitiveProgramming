907a3309dd12a77a75a65594b03c832fa82141b9
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-13 02:50:52 +0900

    区間スケジューリング

--- abc230/D/main.py
+++ abc230/D/main.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3.8
+N, D = map(int, input().split())
+LR = []
+for i in range(N):
+  l, r = map(int, input().split())
+  LR.append((l, r))
+ 
+LR.sort(key=lambda x: (x[1], x[0]), reverse=True)
+ 
+# print(LR)
+l, r = LR.pop()
+sep_l = r
+sep_r = r + D - 1
+cnt = 1
+
+while len(LR) > 0:
+  l, r = LR.pop()
+
+  # print('sep', cnt, (l, r), [sep_l, sep_r], l<=sep_r)
+  if l <= sep_r:
+    continue
+  sep_l = r
+  sep_r = r + D - 1
+  cnt += 1
+
+print(cnt)
