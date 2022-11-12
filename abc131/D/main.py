47108d2c3f14e5c70065afd5e16755467661a6db
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-12 16:18:54 +0900

    貪欲

--- abc131/D/main.py
+++ abc131/D/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+N = int(input())
+T = []
+for i in range(N):
+  a, b = map(int, input().split())
+  T.append((b, -a))
+
+T.sort()
+# print(T)
+
+c = 0
+for t in T:
+  p = -t[1]
+  c += p
+  if t[0] < c:
+    print('No')
+    exit()
+print('Yes')
