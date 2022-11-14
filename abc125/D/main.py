f4960189d8b3df63df5c2eda3ee9a4d5b3514156
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-14 21:35:36 +0900

    アイディア問題

--- abc125/D/main.py
+++ abc125/D/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+absa = [abs(a) for a in A]
+minus_count = len([a for a in A if a < 0])
+if minus_count % 2 == 0:
+  print(sum(absa))
+else:
+  print(sum(absa) - 2 * min(absa))
