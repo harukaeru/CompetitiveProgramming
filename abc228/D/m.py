2c8308439705fd700646b5d6ac7cc3a573a01cf0
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-15 05:32:45 +0900

    Union-Find

---
+++ abc228/D/m.py
@@ -0,0 +1,4 @@
+Q = 2 * (10 ** 5)
+print(Q)
+for i in range(Q):
+    print(1, 1)
--- abc228/D/main.py
+++ abc228/D/main.py
@@ -1,1 +1,29 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env pypy3
+import sys
+sys.setrecursionlimit(300000000)
+A = dict()
+N = 2 ** 20
+Q = int(input())
+TX = []
+for i in range(Q):
+  t,x = map(int, input().split())
+  TX.append((t,x))
+
+parent = {}
+def find(c):
+  if parent.get(c % N, -1) == -1:
+    parent[c % N] = (c + 1) % N
+    return c % N
+  last = find(parent[c % N])
+  parent[c % N] = last
+  return last
+
+for i in range(Q):
+  t,x = TX[i]
+
+  if t == 1:
+    h = x
+    i = find(h)
+    A[i] = x
+  elif t == 2:
+    print(A.get(x % N, -1))
