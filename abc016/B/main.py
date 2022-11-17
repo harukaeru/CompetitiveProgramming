e3ad18cb3f9560aad0a122c6a000ecf7e73b07c6
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-18 00:38:20 +0900

    streak

--- abc016/B/main.py
+++ abc016/B/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+a,b,c= map(int, input().split())
+if a + b == c and a - b == c:
+  print('?')
+elif a + b == c:
+  print('+')
+elif a - b == c:
+  print('-')
+else:
+  print('!')
