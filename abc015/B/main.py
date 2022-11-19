ca1013f38c76391b04cd5b329c49d7ce5ab997be
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-19 23:38:33 +0900

    ランレングス圧縮

--- abc015/B/main.py
+++ abc015/B/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+cnt = 0
+bugs = 0
+for a in A:
+  if a != 0:
+    cnt += 1
+    bugs += a
+
+print((bugs + cnt - 1)// cnt)
--- abc019/B/main.py
+++ abc019/B/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3.8
+s = input()
+
+cur = None
+cnt = 0
+ans = ''
+for i, x in enumerate(s):
+  if cur == x:
+    cnt += 1
+
+  if cur is None:
+    cur = x
+    cnt = 1
+  elif cur != x:
+    ans += cur
+    ans += str(cnt)
+    cur = x
+    cnt = 1
+
+  if i == len(s) - 1:
+    ans += cur
+    ans += str(cnt)
+  
+print(ans)
