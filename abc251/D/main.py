71a3a85cfae1ea06667a862d3abda30dc8a9a17c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-21 19:51:41 +0900

    みずいろもんだい

--- abc251/D/main.py
+++ abc251/D/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+W = int(input())
+
+ans = []
+ans += list(range(1, 100))
+ans += [100 * x for x in range(1, 100)]
+ans += [10000 * x for x in range(1, 100)]
+
+print(len(ans))
+print(*ans) 
