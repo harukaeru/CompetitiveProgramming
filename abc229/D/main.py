7c6e15565097b7c7a25a2870066319f9fb8e28f9
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-24 00:55:46 +0900

    累積和の拡張

--- abc229/D/main.py
+++ abc229/D/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+import bisect
+S = input()
+K = int(input())
+
+T = [0] * (1 + len(S))
+for i in range(len(S)):
+  T[i + 1] = T[i] + (1 if S[i] == '.' else 0)
+
+Q = []
+for t in T:
+  Q.append(max(t - K, 0))
+
+mc = 0
+for i, q in enumerate(Q):
+  j = bisect.bisect_left(T, q)
+  cnt = i - j
+  mc = max(mc, cnt)
+  # print((j,i), cnt)
+print(mc)
