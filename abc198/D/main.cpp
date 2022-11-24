846e0d12fe6fa7f9507160cdf5fb36774853d223
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-25 01:37:50 +0900

    permutations

--- abc198/D/main.cpp
+++ abc198/D/main.cpp
@@ -1,9 +1,67 @@
+#include <algorithm>
 #include <bits/stdc++.h>
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 using namespace std;
 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define pp(v)              \
+  for (auto x : v) {       \
+    std::cout << x << ' '; \
+  }                        \
+  std::cout << endl;
+
 int main() {
+
+  string s1, s2, s3;
+  cin >> s1 >> s2 >> s3;
+
+  set<char> s;
+  for (int i = 0; i < (int)s1.size(); i++) {
+    s.insert(s1.at(i));
+  }
+  for (int i = 0; i < (int)s2.size(); i++) {
+    s.insert(s2.at(i));
+  }
+  for (int i = 0; i < (int)s3.size(); i++) {
+    s.insert(s3.at(i));
+  }
+
+  if (s.size() > 10) {
+    cout << "UNSOLVABLE" << endl;
+    return 0;
+  }
+  vector<char> v;
+  int cnt = 10 - (int)s.size();
+  v.assign(s.begin(), s.end());
+  for (int i = 0; i < cnt; i++) {
+    v.push_back('$' + i);
+  }
+  sort(v.begin(), v.end());
+
+  do {
+    string t1 = s1;
+    string t2 = s2;
+    string t3 = s3;
+
+    for (int i = 0; i < 10; i++) {
+      char x = v[i];
+      char y = i + '0';
+      replace(t1.begin(), t1.end(), x, y);
+      replace(t2.begin(), t2.end(), x, y);
+      replace(t3.begin(), t3.end(), x, y);
+    }
+    // cout << t1 << ' ' << t2 << ' ' << t3 << endl;
+    if (t1[0] == '0' || t2[0] == '0' || t3[0] == '0') {
+      continue;
+    }
+    if (stol(t1) + stol(t2) == stol(t3)) {
+      cout << t1 << endl;
+      cout << t2 << endl;
+      cout << t3 << endl;
+      return 0;
+    }
+  } while (next_permutation(v.begin(), v.end()));
+
+  cout << "UNSOLVABLE" << endl;
   return 0;
 }
--- abc198/D/main.py
+++ abc198/D/main.py
@@ -1,1 +1,45 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env pypy3
+from itertools import permutations
+
+s1 = input()
+s2 = input()
+s3 = input()
+
+s = set(s1).union(set(s2)).union(set(s3))
+if len(s) > 10:
+  print('UNSOLVABLE')
+  exit()
+
+s = list(s) + [''] * (10 - len(s))
+def repl(s, d):
+  r = 0
+  l = len(s)
+  k = 0
+  p = 1
+  while l > 0:
+    t = d[s[l - 1 - k]]
+    r += t * p
+    p *= 10
+    l -= 1
+  if t == 0:
+    return -1111111111111
+  return r
+for x in permutations(s):
+  t1 = s1
+  t2 = s2
+  t3 = s3
+  # print('x', x)
+  d = dict()
+  for i, v in enumerate(x):
+    if not v:
+      continue
+    d[v] = i
+  t1 = repl(t1, d)
+  t2 = repl(t2, d)
+  t3 = repl(t3, d)
+  if t1 + t2 == t3:
+    print(t1)
+    print(t2)
+    print(t3)
+    exit()
+print('UNSOLVABLE')
