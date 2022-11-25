dd705aef45915e56451f3b62f54c5b2ae7ec19ee
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-25 20:44:17 +0900

    クーポンコレクター問題/2

--- abc194/D/main.py
+++ abc194/D/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env pypy3
 N = int(input())
 
-e = 0
-for i in range(1, N):
-  e += N / (N - i)
-print(e)
+v = 0
+for N in range(1, 10000):
+  e = 0
+  for i in range(1, N):
+    e += N / (N - i)
+  if v < e // N:
+    v = e // N
+    print(N, ':', v, ':', e)
