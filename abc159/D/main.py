b95f966d2681df8249a32854b2e92ab04797036a
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-26 10:53:52 +0900

    先にトータルを計算

--- abc159/D/main.py
+++ abc159/D/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+from collections import Counter
+from math import comb
+
+N = int(input())
+A = list(map(int, input().split()))
+
+counter = Counter(A)
+tot = sum(comb(v, 2) for v in counter.values())
+
+for a in A:
+  k = counter[a]
+  print(tot - comb(k, 2) + comb(k - 1, 2))
