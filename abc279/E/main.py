5c4a870a2b6bf440f04996068c5b2efa41aad1f4
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-27 16:10:41 +0900

    position

--- abc279/E/main.py
+++ abc279/E/main.py
@@ -1,73 +1,23 @@
 #!/usr/bin/env python3.8
-from scipy.sparse import lil_matrix
-from scipy.sparse.linalg import inv
-import numpy as np
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
-B = [0] * N
-B[0] = 1
-B = np.array(B)
 
-matrixes = []
-invs = []
-for i in range(M):
-  a = A[i]
-  b = a - 1
-  x = lil_matrix((N, N))
-  for j in range(N):
-    if j == a:
-      x[j,b] = 1
-    elif j == b:
-      x[j,a] = 1
-    else:
-      x[j,j] = 1
-  matrixes.append(x)
-  invs.append(inv(x))
+B = list(range(0, N + 1))
 
-invs.reverse()
-print('matrixes')
-for ma in matrixes:
-  print(ma.todense())
+for a in A:
+  B[a], B[a + 1] = B[a+1],B[a]
 
-cinvs = [None] * (len(invs))
-cinvs[0] = invs[0]
-for i in range(1, len(invs)):
-  cinvs[i] = invs[i].multiply(cinvs[i - 1])
+pos = [None] * (N + 1)
+for i in range(1, N+1):
+  pos[B[i]] = i
 
-cmatrixes = [None] * (len(matrixes))
-cmatrixes[0] = matrixes[0]
-for i in range(1, len(matrixes)):
-  cmatrixes[i] = matrixes[i].multiply(cmatrixes[i - 1])
+B = list(range(0, N + 1))
 
-
-print('B', B)
-for cc in cmatrixes:
-  print(cc.todense())
-
-C = cmatrixes[-1].multiply(B)
-
-print('C', C)
-print('?')
-for ma in matrixes:
-  print(ma.todense())
-  print('-----------')
-
-print('-----------------LL-')
-for i in range(M):
-  if i == 0:
-    x = cinvs[0].multiply(C)
-    print(x.todense())
+for a in A:
+  if B[a] == 1:
+    print(pos[B[a + 1]])
+  elif B[a + 1] == 1:
+    print(pos[B[a]])
   else:
-    x = cmatrixes[i - 1].multiply(cinvs[i]).multiply(C)
-    print(x.todense())
-# print(matrixes)
-
-# ナイーブ解
-# for i in range(M):
-#   for j in range(M):
-#     if i == j:
-#       continue
-#     a = A[j]
-#     a -= 1
-#     B[a], B[a + 1] = B[a + 1], B[a]
-#   print(B.index(1) + 1)
+    print(pos[1])
+  B[a], B[a + 1] = B[a+1],B[a]
