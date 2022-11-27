012599919400cb3343693f8ce2cb98d8c00c9cd5
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-27 19:35:42 +0900

    最初に惨敗状態を作っておき、何が必要かを貪欲

--- abc187/D/main.py
+++ abc187/D/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+N = int(input())
+non_cheers = []
+diffs = []
+for i in range(N):
+  a, b = map(int, input().split())
+  non_cheers.append(-a)
+  diffs.append(a + b + a)
+
+nc = sum(non_cheers)
+diffs.sort(reverse=True)
+
+cnt = 0
+for d in diffs:
+  nc += d
+  cnt += 1
+  if nc > 0:
+    print(cnt)
+    exit()
