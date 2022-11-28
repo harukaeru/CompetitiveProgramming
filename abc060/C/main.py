37deb4744819fade1d1fc082c9bb0f1fd82a27ac
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-28 23:10:31 +0900

    minの総和

--- abc060/C/main.py
+++ abc060/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+N, T = map(int, input().split())
+times = list(map(int, input().split()))
+
+s = 0
+for i in range(N - 1):
+  t = times[i]
+  nt = min(t + T, times[i + 1])
+  d = nt - t
+  s += d
+
+s += T
+print(s)
