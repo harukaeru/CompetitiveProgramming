5b9d8840e3c50d0795ebb0db21773904c5d142d9
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-28 23:51:38 +0900

    ABC 灰diffコンプリート/2

--- abc012/C/main.py
+++ abc012/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+N = int(input())
+
+s = 0
+for i in range(1, 10):
+  for j in range(1, 10):
+    s += i * j
+
+rest = s - N
+
+for i in range(1, 10):
+  for j in range(1, 10):
+    if i * j == rest:
+      print(i, 'x', j)
