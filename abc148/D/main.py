c03e55ccdf51ae0d1f154fac7a2d5d257037e682
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-28 23:48:34 +0900

    ABC 灰diffコンプリート

--- abc148/D/main.py
+++ abc148/D/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+t = 1
+cnt = 0
+for i in range(N):
+  a = A[i]
+  if a == t:
+    cnt += 1
+    t += 1
+
+if cnt == 0:
+  print(-1)
+  exit()
+
+print(N - cnt)
