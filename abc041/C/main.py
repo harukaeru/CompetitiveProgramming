dafc3cb56f1e26b7895603402873ab8b149a3e70
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-30 01:27:08 +0900

    実質灰diff

--- abc041/C/main.py
+++ abc041/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+B = []
+for i in range(N):
+  B.append((A[i], i))
+
+B.sort(reverse=True)
+
+for i in range(N):
+  print(B[i][1] + 1)
