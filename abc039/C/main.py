478468b5677ed6527a17be2baa3c1023758ae363
harukaeru <nekoneko.myaomyao@gmail.com>
2022-11-30 01:45:24 +0900

    C埋め

--- abc039/C/main.py
+++ abc039/C/main.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3.8
+S = input()
+
+k = 'WBWBWWBWBWBW'
+k = k * 3
+
+i = k.index(S)
+ranks = {
+  0: 'Do', 
+  2: 'Re', 
+  4: 'Mi', 
+  5: 'Fa',
+  7: 'So',
+  9: 'La',
+  11: 'Si'
+}
+
+pos = None
+for i in range(0, 13):
+  l = k[i:len(S) + i]
+  if S == l:
+    pos = i
+    break
+
+print(ranks[pos])
--- abc055/C/main.py
+++ abc055/C/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3.8
+N, M = map(int, input().split())
+
+if N * 2 >= M:
+  print(M // 2)
+  exit()
+
+cnt = N
+M = M - N * 2
+M //= 4
+cnt += M
+print(cnt)
