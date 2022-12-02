a17ff46a304e35b2db5884e56ede5b90969995d6
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-02 13:26:19 +0900

    ワンペナ

--- abc064/C/main.py
+++ abc064/C/main.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+def color(v):
+  if v <= 399:
+    return 1
+  if v <= 799:
+    return 2
+  if v <= 1199:
+    return 3
+  if v <= 1599:
+    return 4
+  if v <= 1999:
+    return 5
+  if v <= 2399:
+    return 6
+  if v <= 2799:
+    return 7
+  if v <= 3199:
+    return 8
+  return None
+
+
+free_count = 0
+orig = set()
+for i in range(N):
+  a = A[i]
+  col = color(a)
+  if col is None:
+    free_count += 1
+  else:
+    orig.add(col)
+
+mi = max(len(orig), 1)
+ma = len(orig) + free_count
+print(mi, ma)
--- abc066/C/main.py
+++ abc066/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+from collections import deque
+N = int(input())
+A = list(map(int, input().split()))
+
+q = deque()
+
+for i in range(N):
+  if i % 2 == 0:
+    q.append(A[i])
+  else:
+    q.appendleft(A[i])
+
+if N % 2 == 0:
+  print(*q)
+else:
+  q.reverse()
+  print(*q)
