cd0aab9d2bdf4b0f6b40305a1e5fbc090f4b8969
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-02 13:39:19 +0900

    bit全列挙

--- abc045/C/main.py
+++ abc045/C/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.8
+S = input()
+
+tot = 0
+for i in range(2 ** (len(S) - 1)):
+  s = ''
+  for j in range(len(S) - 1):
+    s += S[j]
+    if i & (1 << j) != 0:
+      s += '+'
+  
+  s += S[-1]
+
+  tot += eval(s)
+      
+  # print(s)
+print(tot)
