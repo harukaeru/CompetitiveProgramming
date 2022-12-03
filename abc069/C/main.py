edb2d32443d9983aaef81480133f0d15b4abf545
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-03 14:48:49 +0900

    C埋め

--- abc069/C/main.py
+++ abc069/C/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+c4 = 0
+c2 = 0
+c1 = 0
+for i in range(N):
+  a = A[i]
+  if a % 4 == 0:
+    c4 +=1
+  elif a % 2 == 0:
+    c2 += 1
+  else:
+    c1 += 1
+
+# print(c1, c2, c4)
+if c4 + 1 == c1 and N == c4 + c1:
+  print('Yes')
+elif c4 < c1:
+  print('No')
+else:
+  print('Yes')
--- abc076/C/main.py
+++ abc076/C/main.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3.8
+S = input()
+T = input()
+
+ans = None
+for i in range(len(S) - len(T), -1, -1):
+  ok = True
+  # print('i', i)
+  for j in range(len(T)):
+    s = S[i + j]
+    t = T[j]
+    # print('   ', (s, t))
+    if s == '?':
+      continue
+    elif s == t:
+      continue
+
+    ok = False
+    break
+  if ok:
+    ans = S[:i] + T + S[i + len(T):]
+    break
+
+if ans is None:
+  print('UNRESTORABLE')
+else:
+  print(ans.replace('?', 'a'))
