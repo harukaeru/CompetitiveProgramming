819706bedef8598bef2523e0b609fe3a49ec6c65
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-04 01:42:38 +0900

    bit全探索

--- abc029/C/main.py
+++ abc029/C/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+N = int(input())
+
+anses = []
+for i in range(3 ** N):
+  p = i
+  s = ''
+  for j in range(N):
+    if p % 3 == 0:
+      s += 'a'
+    elif p % 3 == 1:
+      s += 'b'
+    else:
+      s += 'c'
+    p //= 3
+  anses.append(s)
+
+anses.sort()
+for a in anses:
+  print(a)
