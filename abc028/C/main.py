51085cdcf0100e06d15c55c6e2c96ccdb721c8ee
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-04 16:03:34 +0900

    区間和など

--- abc028/C/main.py
+++ abc028/C/main.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3.8
+X=list(map(int,input().split()))
+print(max(X[0]+X[3]+X[4],X[1]+X[2]+X[4]))
--- abc154/D/main.py
+++ abc154/D/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.8
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+
+es = []
+for i in range(N):
+  p = P[i]
+  e = (p * (p + 1) / 2) / p
+  es.append(e)
+# print(es)
+
+me = sum(es[:K])
+cur = me
+for i in range(K, N):
+  cur = cur + es[i] - es[i - K]
+  me = max(me, cur)
+print(me)
