fa835ac82e5ff8a5c4b6960dd29fc86bacb661a3
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-05 01:10:03 +0900

    LCM

--- abc070/C/main.py
+++ abc070/C/main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3.8
+import math
+
+
+N = int(input())
+T = []
+for i in range(N):
+  t = int(input())
+  T.append(t)
+
+x = 1
+for i in range(N):
+  t = T[i]
+  x = (t * x) // math.gcd(t, x)
+print(x)
