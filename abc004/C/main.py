a098c0122e47c2562cf3a9741c314b515f257f02
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-05 23:45:44 +0900

    C埋め

--- abc004/C/main.py
+++ abc004/C/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+N = int(input())
+
+N %= 30
+
+x = ['1', '2', '3', '4', '5', '6']
+for i in range(N):
+  x[(i % 5)], x[(i % 5) + 1] = x[(i % 5) + 1], x[(i % 5)]
+
+print(''.join(x))
