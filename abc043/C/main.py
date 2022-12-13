8264f7bbf7f8818c2fd179c21e766cc943514fe2
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-14 01:28:25 +0900

    定義域

--- abc043/C/main.py
+++ abc043/C/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+s = sum(A)
+t = 0
+for a in A:
+  t += a * a
+
+def calc(x):
+  return t - 2 * x * s + N * x * x
+
+m = 1e18
+for i in range(-100, 101):
+  c = calc(i)
+  # print(i, c)
+  if m > c:
+    m = c
+
+print(m)
