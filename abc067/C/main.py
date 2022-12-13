83a8c267f4cc46a53c6f2af5387808e3e061ae78
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-13 21:23:35 +0900

    累積和

--- abc067/C/main.py
+++ abc067/C/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+tot = sum(A)
+B = [0] + list(itertools.accumulate(A))
+# print(B)
+mi = 1e19
+for i in range(1, N):
+  x = B[i]
+  y = tot - x
+  mi = min(mi, abs(x - y))
+print(mi)
