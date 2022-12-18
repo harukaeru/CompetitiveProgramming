7bb9555e98539c4566a9156725f1b8383fa56ff6
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-18 19:51:41 +0900

    swapのくりかえし

--- abc072/D/main.py
+++ abc072/D/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+N = int(input())
+P = list(map(int, input().split()))
+
+cnt = 0
+for i in range(1, N + 1):
+  if i == N and i == P[i - 1]:
+    P[i - 1], P[i - 2] = P[i - 2], P[i - 1]
+    cnt += 1
+  elif i == P[i - 1]:
+    P[i - 1], P[i] = P[i], P[i - 1]
+    cnt += 1
+print(cnt)
