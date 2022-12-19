7f2ae23291130fb4c80daae4ad149b9c0902858c
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-19 19:56:47 +0900

    解説の再帰をやる

--- abc266/E/main.py
+++ abc266/E/main.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3.8
-import math
-
-
 N = int(input())
 
 cache = {}
-accums = [0, 21, 20, 18, 15, 11, 6]
 def evaluate(K):
   if K == 1:
     return 7 / 2
   if cache.get(K):
     return cache[K]
 
-  good_e = evaluate(K - 1)
-  idx = math.ceil(good_e)
-  big_e = accums[idx]
-  cnt = (6 - idx + 1)
-  small_e = good_e * (6 - cnt)
-  cache[K] = (big_e + small_e) /6
+  prev = evaluate(K - 1)
+
+  e = 0
+  for i in range(1, 7):
+    e += max(i, prev)
+  e /= 6
+  cache[K] = e
   return cache[K]
 
 
