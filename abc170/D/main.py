6988b3db23a399cf709a7adb934e145d0f168fd7
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-22 02:22:55 +0900

    調和級数

--- abc170/D/main.py
+++ abc170/D/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3.8
+from collections import Counter
+
+N = int(input())
+A = list(map(int, input().split()))
+
+counter = Counter(A)
+S = set(A)
+A.sort()
+M = max(A)
+# print(A)
+
+cnt = 0
+ng = set()
+for i in range(N):
+  a = A[i]
+  if counter[a] >= 2:
+    ng.add(a)
+  for b in range(2 * a, M + 1, a):
+    if b in S:
+      ng.add(b)
+
+print(len(S) - len(ng))
