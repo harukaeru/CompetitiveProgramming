18af01e6b4d49c78b9abf02cf6b9093cfaa9c4c5
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-22 01:20:54 +0900

    累積和

--- abc183/D/main.py
+++ abc183/D/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N, W = map(int, input().split())
+M = 2 * (10 ** 5) + 1
+times = [0] * (M)
+
+for i in range(N):
+  s,t,p = map(int, input().split())
+  times[s] += p
+  times[t] -= p
+
+A = itertools.accumulate(times)
+if max(A) > W:
+  print('No')
+else:
+  print('Yes')
