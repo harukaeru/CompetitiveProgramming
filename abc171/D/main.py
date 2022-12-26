d2c5cb200c190ca6e807a5e008b0b823c5928367
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-27 01:14:47 +0900

    bucket

--- abc171/D/main.py
+++ abc171/D/main.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3.8
+from collections import Counter
+
+
+N = int(input())
+A = list(map(int, input().split()))
+Q = int(input())
+
+counter = Counter(A)
+
+s = 0
+for k,v in counter.items():
+  s += k * v
+for i in range(Q):
+  b,c=map(int,input().split())
+  cnt = counter[b]
+  counter[b] = 0
+  counter[c] += cnt
+  s -= b * cnt
+  s += c * cnt
+
+  print(s)
