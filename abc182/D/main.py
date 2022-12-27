d5ac77e2fe7004c1aa2e3750054634a602d0a752
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-28 00:48:22 +0900

    累積和

--- abc182/D/main.py
+++ abc182/D/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+B = list(itertools.accumulate(A))
+M = [0]
+for i in range(N):
+  M.append(max(B[i], M[i]))
+
+s = 0
+t = 0
+for i in range(N):
+  m = s + M[i + 1]
+  t = max(t, m)
+  s += B[i]
+print(t)
