de6b34c1a440d3bfbe01963e51b690e6b28546df
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-01 00:58:16 +0900

    整数

--- abc190/D/main.py
+++ abc190/D/main.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env pypy3
+# 素因数分解
+import math
+
+N = int(input())
+N *= 2
+ma = int(math.sqrt(N))
+
+fact = set()
+for i in range(1, ma + 1):
+  if N % i == 0:
+    fact.add(i)
+    fact.add(N // i)
+
+cnt = 0
+for n in fact:
+  p = N // n - n + 1
+  if p % 2 == 0:
+    cnt += 1
+
+print(cnt)
