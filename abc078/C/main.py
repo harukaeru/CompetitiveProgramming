b8373197ecbca5388cb6d17d594439809a574509
harukaeru <nekoneko.myaomyao@gmail.com>
2022-12-31 23:41:05 +0900

    無限等比級数

--- abc078/C/main.py
+++ abc078/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+N, M = map(int, input().split())
+
+t = (N - M) * 100
+t += M * 1900
+
+denom = 2 ** M
+r = (denom - 1) / denom
+a = 1 / denom
+p = a / (1 - r)
+
+ans = t * p / (1 - r)
+print(int(ans))
