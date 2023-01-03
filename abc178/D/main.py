bf95c741029fedad21c56131007c39a83f1b4c84
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-03 19:33:06 +0900

    メモ化再帰

--- abc178/D/main.py
+++ abc178/D/main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3.8
+MOD = 10 ** 9 + 7
+cache = {}
+def f(x):
+  if x <= 2:
+    return 0
+
+  if x <= 5:
+    return 1
+  
+  if cache.get(x):
+    return cache[x]
+
+  s = 1
+  for i in range(3, x - 3 + 1):
+    s += f(i) % MOD
+  cache[x] = s
+  return s
+
+S = int(input())
+print(f(S) % MOD)
