df4d8febfcff5425e612ee0251c35059b0b46cb6
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-06 19:12:41 +0900

    一次不等式

--- abc180/D/main.py
+++ abc180/D/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+X,Y,A,B = map(int, input().split())
+
+cnt = 0
+while X * A <= B:
+  X *= A
+  if X >= Y:
+    print(cnt)
+    exit()
+  cnt += 1
+
+c = (Y - X) // B
+if X + c * B >= Y:
+  c -= 1
+
+print(cnt + c)
