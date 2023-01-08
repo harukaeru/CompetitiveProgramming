2005b2c8638c4e451188127009132c9b409cfe14
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-08 22:39:20 +0900

    コードを短くした

--- abc239/E/main.py
+++ abc239/E/main.py
@@ -10,6 +10,8 @@ X = list(map(int, input().split()))
 graph = defaultdict(list)
 for i in range(N - 1):
   a,b = map(int, input().split())
+  a -= 1
+  b -= 1
   graph[a].append(b)
   graph[b].append(a)
 
@@ -19,62 +21,23 @@ for i in range(Q):
   v,k = map(int, input().split())
   queries.append((v, k))
 
-class Node:
-  def __init__(self, parentNode, num, strength):
-    self.parentNode = parentNode
-    self.num = num
-    self.strength = strength
-    self.children = []
-  
-  def __repr__(self):
-    return f'<cur: {self.num} | parent: {self.parentNode.num if self.parentNode else "None"}>'
-  
-  def decide_strength_ranking(self, strength_ranking):
-    self.strength_ranking = strength_ranking
+weights_list = [[] for i in range(N)]
 
 # DFSの初期化
 seen = set()
-seen.add(1)
-ranks = defaultdict(list)
+seen.add(0)
 
-# Node周りの設定
-startingNode = Node(None, 1, X[0])
-ranks[0].append(startingNode)
-cached_nodes = [None] * N
-cached_nodes[0] = startingNode
-
-def dfs(v, depth=0):
-  for nex in graph[v.num]:
+def dfs(v):
+  weights_list[v] += [X[v]]
+  for nex in graph[v]:
     if nex not in seen:
       seen.add(nex)
-      nexNode = Node(v, nex, X[nex - 1])
-      v.children.append(nexNode)
+      dfs(nex)
+      weights_list[v] += weights_list[nex]
+  weights_list[v] = sorted(weights_list[v], reverse=True)[:STRENGTH_RANKING_LIMIT]
 
-      ranks[depth + 1].append(nexNode)
-      cached_nodes[nex - 1] = nexNode
-
-      dfs(nexNode, depth=depth+1)
-
-dfs(startingNode)
-
-# 各ノードについてランキングを作成する。RMQ風味。
-keys = list(ranks.keys())
-max_rank = max(keys)
-for k in range(max_rank, -1, -1):
-  for node in ranks[k]:
-    new_ranking = []
-    for child in node.children:
-      new_ranking += child.strength_ranking[:]
-    new_ranking += [node.strength]
-    new_ranking.sort(reverse=True)
-    new_ranking = new_ranking[:STRENGTH_RANKING_LIMIT]
-
-    node.decide_strength_ranking(new_ranking)
-
-
-# for i in range(N):
-#   print(cached_nodes[i].array)
+dfs(0)
 
 for query in queries:
   v, k = query
-  print(cached_nodes[v - 1].strength_ranking[k - 1])
+  print(weights_list[v - 1][k - 1])
