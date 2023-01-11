47f7399e7ab1aa5d6d0ef9e83278a409d774cd8e
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-12 01:19:39 +0900

    ランレングス圧縮をつかったやりかた

--- abc124/D/main.py
+++ abc124/D/main.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python3.8
 from collections import deque
+# https://qiita.com/Kept1994/items/e9179d1dd7c6455d6883
+from itertools import groupby
+
+# RUN LENGTH ENCODING str -> list(tuple())
+# example) "aabbbbaaca" -> [('a', 2), ('b', 4), ('a', 2), ('c', 1), ('a', 1)]
+def runLengthEncode(S: str):
+    grouped = groupby(S)
+    res = []
+    for k, v in grouped:
+        res.append((k, int(len(list(v)))))
+    return res
+
+# RUN LENGTH DECODING list(tuple()) -> str
+# example) [('a', 2), ('b', 4), ('a', 2), ('c', 1), ('a', 1)] -> "aabbbbaaca"
+def runLengthDecode(L) -> str:
+    res = ""
+    for c, n in L:
+        res += c * int(n)
+    return res
+
+# RUN LENGTH ENCODING str -> str
+# example) "aabbbbaaca" -> "a2b4a2c1a1"
+def runLengthEncodeToString(S: str) -> str:
+    grouped = groupby(S)
+    res = ""
+    for k, v in grouped:
+        res += k + str(len(list(v)))
+    return res
 
 
 N, K = map(int, input().split())
 S = input()
 
-next_starts = deque()
-l = 0
+# print(runLengthEncode(S))
+rle = runLengthEncode(S)
+
+tot = 0
 r = 0
+l = 0
 zero_cnt = 0
-ml = 0
-while l < N:
-  k = r
-  while r < N:
-    if zero_cnt == K:
-      while r < N and S[r] != '0':
-        k = r
-        r += 1
-      break
-      
+ans = 0
+while r < len(rle):
+  tot += rle[r][1]
+  if rle[r][0] == '0':
+    zero_cnt += 1
 
-    if S[r] == '1':
-      k = r
-      r += 1
-      continue
-
-    if S[r] == '0':
-      zero_cnt += 1
-      while r < N and S[r] == '0':
-        k = r
-        r += 1
-      next_starts.append(r)
-
-  r = k
-  ml = max(ml, r - l + 1)
-  if len(next_starts):
-    l = next_starts.popleft()
-    zero_cnt -= 1
-  else:
-    if l == r:
-      r += 1
+  while K < zero_cnt:
+    tot -= rle[l][1]
+    if rle[l][0] == '0':
+      zero_cnt -= 1
     l += 1
-print(ml)
+  ans = max(ans, tot)
+
+  r += 1
+
+print(ans)
