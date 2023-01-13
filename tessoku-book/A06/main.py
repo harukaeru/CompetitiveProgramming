009ea1fbfb87276eb9dc34027e68e9c30367e03c
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-13 21:40:25 +0900

    累積和

--- tessoku-book/A06/main.py
+++ tessoku-book/A06/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N,Q = map(int, input().split())
+A = list(map(int, input().split()))
+B = [0] + list(itertools.accumulate(A))
+
+for i in range(Q):
+  l,r = map(int, input().split())
+  print(B[r]-B[l-1])
