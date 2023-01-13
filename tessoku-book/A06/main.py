7b24fe50a74807031cac7a49a87d40425b51382e
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-13 21:52:35 +0900

    たのしい累積和

--- tessoku-book/A06/main.py
+++ tessoku-book/A06/main.py
@@ -4,7 +4,9 @@ import itertools
 
 N,Q = map(int, input().split())
 A = list(map(int, input().split()))
-B = [0] + list(itertools.accumulate(A))
+B = [0] * (1 + N)
+for i in range(1, N + 1):
+  B[i] = A[i - 1] + B[i - 1]
 
 for i in range(Q):
   l,r = map(int, input().split())
--- tessoku-book/B06/main.py
+++ tessoku-book/B06/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N = int(input())
+A = list(map(int, input().split()))
+B = [0] + list(itertools.accumulate(A))
+Q = int(input())
+for i in range(Q):
+  l,r=map(int, input().split())
+  ok_cnt = B[r] - B[l - 1]
+  tot = (r - l + 1)
+  ng_cnt = tot - ok_cnt
+  if ok_cnt > ng_cnt:
+    print('win')
+  elif ok_cnt == ng_cnt:
+    print('draw')
+  else:
+    print('lose')
