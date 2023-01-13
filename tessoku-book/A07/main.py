338bf8f96d475d59a8b4f93b1fdac3cae2587236
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-13 21:55:31 +0900

    1次元累積和

--- tessoku-book/A07/main.py
+++ tessoku-book/A07/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+D = int(input())
+N = int(input())
+
+days = [0] * (2 + D)
+for i in range(N):
+  l,r = map(int, input().split())
+  days[l] += 1
+  days[r + 1] -= 1
+
+A  =list(itertools.accumulate(days))
+for i in range(1, D + 1):
+  print(A[i])
