1f08b7e3742ed5097a3b84fae147f9397d881c3f
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-13 22:00:40 +0900

    他の累積和

--- tessoku-book/B07/main.py
+++ tessoku-book/B07/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+T = int(input())
+N = int(input())
+
+times = [0] * (2 + T)
+for i in range(N):
+  l, r = map(int, input().split())
+  times[l] += 1
+  times[r] -= 1
+
+# print(times)
+times = list(itertools.accumulate(times))
+for i in range(0, T):
+  print(times[i])
