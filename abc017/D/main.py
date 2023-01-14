6c16ca6c53934bb98c654efb9eecb3967d1b0f92
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-14 16:15:01 +0900

    全探索

--- abc017/D/main.py
+++ abc017/D/main.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3.8
+N,M=map(int, input().split())
+for i in range(N):
--- abc166/D/main.py
+++ abc166/D/main.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3.8
+X = int(input())
+
+for a in range(-120, 121):
+  for b in range(-120, 121):
+    if a ** 5 - b ** 5 == X:
+      print(a, b)
+      exit()
