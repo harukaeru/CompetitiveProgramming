b68d4700343b3b165d0996c8e714965def7010d6
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-15 14:52:36 +0900

    両端

--- tessoku-book/A10/main.py
+++ tessoku-book/A10/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+from itertools import accumulate
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+L = [0] + list(accumulate(A, lambda a, b: max(a, b)))
+
+A.reverse()
+R = [0] + list(accumulate(A, lambda a, b: max(a, b)))
+R.reverse()
+
+D = int(input())
+for i in range(D):
+  l,r = map(int, input().split())
+  ml = L[l - 1]
+  mr = R[r]
+  print(max(ml, mr))
