faa364f7564b0b760d3d9f8c2afb2ae6b047c474
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-18 17:05:49 +0900

    連立一次方程式

--- abc133/D/main.py
+++ abc133/D/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+d = 0
+for i in range(N - 1):
+  if i % 2 == 0:
+    d += 2 * A[i]
+  else:
+    d -= 2 * A[i]
+
+x1 = (d + 2 * A[N - 1]) // 2
+anses = [x1]
+x = x1
+
+for i in range(N - 1):
+  a = A[i]
+  x = 2 * a - x
+  anses.append(x)
+print(*anses)
