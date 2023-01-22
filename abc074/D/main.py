d8ab3abaeaaa41f3613fd8ffbcc2c6bab34e18d0
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-23 02:55:16 +0900

    ワーシャルフロイドの経路復元

--- abc074/D/main.py
+++ abc074/D/main.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = []
+B = []
+for i in range(N):
+  a = list(map(int, input().split()))
+  b = a[:]
+  A.append(a)
+  B.append(b)
+
+via = []
+for i in range(N):
+  d = [i] * N
+  via.append(d)
+
+for k in range(N):
+  for i in range(N):
+    for j in range(N):
+      if i == k or k == j:
+        continue
+      if A[i][j] >= B[i][k] + B[k][j]:
+        via[i][j] = k
+        B[i][j] = min(B[i][j], B[i][k] + B[k][j])
+
+# for v in via:
+#   print(v)
+
+ans = 0
+for i in range(N):
+  for j in range(N):
+    if A[i][j] != B[i][j]:
+      print('-1')
+      exit()
+    if i == via[i][j]:
+      ans += B[i][j]
+print(ans // 2)
