3ff4c30c6be5e670f5aff3845503ef1a54ad003d
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-23 22:19:47 +0900

    ダイクストラ法の練習

--- abc012/D/main.py
+++ abc012/D/main.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3.8
+from collections import defaultdict
+import heapq
+
+N,M= map(int, input().split())
+
+G = defaultdict(list)
+for i in range(M):
+  a,b,t=map(int, input().split())
+  a -= 1
+  b -= 1
+  G[a].append((t, b))
+  G[b].append((t, a))
+
+def dijkstra(s):
+  pq = [(0, s)]
+  costs = [1e18] * N
+  costs[s] = 0
+
+  while len(pq) > 0:
+    c, pos = heapq.heappop(pq)
+    if costs[pos] < c:
+      continue
+    
+    for nex_cost, nex in G[pos]:
+      cand = nex_cost + costs[pos]
+      if costs[nex] > cand:
+        costs[nex] = cand
+        heapq.heappush(pq, (cand, nex))
+  return costs
+
+md = 1e18
+for i in range(N):
+  costs = dijkstra(i)
+  md = min(md, max(costs))
+print(md)
