5d7233b3ec0b27f99ac466a02e98c8e0f2b26944
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-23 22:38:02 +0900

    もう少し綺麗にダイクストラ法

--- libs/dijkstra.py
+++ libs/dijkstra.py
@@ -1,16 +1,4 @@
-#!/usr/bin/env python3.8
-from collections import defaultdict
 import heapq
-
-N,M= map(int, input().split())
-
-G = defaultdict(list)
-for i in range(M):
-  a,b,t=map(int, input().split())
-  a -= 1
-  b -= 1
-  G[a].append((t, b))
-  G[b].append((t, a))
 
 def dijkstra(start):
   p_queue = [(0, start)]
@@ -32,8 +20,4 @@ def dijkstra(start):
         heapq.heappush(p_queue, (d_from_start_to_nex, nex_node))
   return dists
 
-md = 1e18
-for i in range(N):
-  costs = dijkstra(i)
-  md = min(md, max(costs))
-print(md)
+
