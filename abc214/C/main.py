e76c07283b5b68cb5391e26e13c28ce5b7e30721
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-25 16:26:08 +0900

    ダイクストラ法

--- abc214/C/main.py
+++ abc214/C/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3.8
+import heapq
+
+
+N = int(input())
+S = list(map(int, input().split()))
+T = list(map(int, input().split()))
+
+pq = []
+for i, t in enumerate(T):
+  heapq.heappush(pq, (t, i))
+
+while pq:
+  t, i = heapq.heappop(pq)
+  if T[i] < t:
+    continue
+
+  nex = (i + 1) % N
+  if T[nex] > S[i] + t:
+    T[nex] = S[i] + t
+    heapq.heappush(pq, (T[nex], nex))
+
+for t in T:
+  print(t)
--- abc252/E/main.py
+++ abc252/E/main.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3.8
+from collections import defaultdict
+import heapq
+
+N,M=map(int, input().split())
+edges = defaultdict(list)
+for i in range(M):
+  a,b,c= map(int, input().split())
+  a-=1
+  b-=1
+  edges[a].append((c, b, i))
+  edges[b].append((c, a, i))
+
+dists = [1e18] * N
+s = 0
+dists[s] = 0
+pq = [(0, s, None)]
+
+route = [-1] * N
+while pq:
+  c, v, i = heapq.heappop(pq)
+  if dists[v] < c:
+    continue
+  for edge in edges[v]:
+    nc, nv, ni = edge
+    candidate = dists[v] + nc
+    if dists[nv] > candidate:
+      dists[nv] = candidate
+      route[nv] = ni
+      heapq.heappush(pq, (dists[nv], nv, ni))
+
+
+route = [r for r in route if r >= 0]
+print(*[r + 1 for r in route])
