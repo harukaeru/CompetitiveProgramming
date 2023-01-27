e0841d10420b51a51ea233eb425187c4b944b8dc
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-27 21:30:09 +0900

    最小全域木

---
+++ AOJ/GRL2/A/main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3.8
+# https://note.nkmk.me/python-union-find/
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+v,e=map(int, input().split())
+edges = []
+for i in range(e):
+  s,t,w=map(int, input().split())
+  edges.append((w, (s, t)))
+
+edges.sort()
+
+uf = UnionFind(v)
+total = 0
+# print(edges)
+for i in range(e):
+  cost, (s, t) = edges[i]
+
+  if uf.same(s, t):
+    continue
+  total += cost
+  uf.union(s, t)
+print(total)
+  
