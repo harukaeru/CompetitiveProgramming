c73c624d3b956d6220ac208b00e794922a59efa9
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-30 23:16:03 +0900

    2分探索

--- tessoku-book/A14/main.py
+++ tessoku-book/A14/main.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3.8
+from bisect import bisect_left
+
+
+N,K=map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+AB = []
+for i in range(N):
+  for j in range(N):
+    AB.append(A[i] + B[j])
+
+CD = []
+for i in range(N):
+  for j in range(N):
+    CD.append(C[i] + D[j])
+
+AB.sort()
+CD.sort()
+
+ok = False
+for i in range(len(AB)):
+  ab = AB[i]
+  cd = K - ab
+  x = bisect_left(CD, cd)
+  if x == len(CD):
+    continue
+  if CD[x] == cd:
+    ok = True
+    break
+
+if ok:
+  print('Yes')
+else:
+  print('No')
