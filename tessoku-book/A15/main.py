2eb39a9ef5c70b59196a3f82d574b98836100bfb
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-31 17:35:20 +0900

    二分探索

--- tessoku-book/A15/main.py
+++ tessoku-book/A15/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+from bisect import bisect_left
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+C = list(set(A))
+C.sort()
+
+B = []
+for a in A:
+  idx = bisect_left(C, a)
+  B.append(idx + 1)
+  
+print(*B)
