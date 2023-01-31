658824bd98098b760a7f1ac983632f4f908fc6e5
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-31 18:15:52 +0900

    鉄則

--- tessoku-book/A17/main.py
+++ tessoku-book/A17/main.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = [1e18, 1e18] + list(map(int, input().split()))
+B = [1e18, 1e18, 1e18] + list(map(int, input().split()))
+
+dp = [1e18] * (1 + N)
+dp[1] = 0
+
+for i in range(2, 1 + N):
+  dp[i] = min(dp[i], dp[i - 1] + A[i])
+  dp[i] = min(dp[i], dp[i - 2] + B[i])
+
+routes = [N]
+d = dp[N]
+i = N
+while i > 1:
+  if dp[i] == dp[i - 1] + A[i]:
+    routes.append(i - 1)
+    i -= 1
+  else:
+    routes.append(i - 2)
+    i -= 2
+  
+routes.reverse()
+print(len(routes))
+print(*routes)
--- tessoku-book/A18/main.py
+++ tessoku-book/A18/main.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3.8
+N,S=map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = []
+for i in range(N + 1):
+  d = [False] * (1 + S)
+  dp.append(d)
+
+dp[0][0] = True
+
+for i in range(1, N + 1):
+  a = A[i - 1]
+  for j in range(0, S + 1):
+    dp[i][j] = dp[i][j] or dp[i - 1][j]
+    if j + a <= S:
+      dp[i][j + a] = dp[i][j + a] or dp[i - 1][j]
+
+if dp[N][S]:
+  print('Yes')
+else:
+  print('No')
