7493d31821e65b06c8630f25083e333504cd9de1
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-02 23:32:18 +0900

    Simple DFS

--- abc026/C/main.py
+++ abc026/C/main.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3.8
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(300000) 
+
+
+N = int(input())
+G = defaultdict(list)
+for i in range(N - 1):
+  num = i + 2
+  b = int(input())
+  G[b].append(num)
+
+
+# print(G)
+
+salaries = [None] * (2 + N)
+
+def dfs(s):
+  if len(G[s]) == 0:
+    salaries[s] = 1
+    return
+
+  max_s = 0
+  min_s = 1e18
+  if len(G[s]) == 0:
+    min_s = 0
+
+  for child in G[s]:
+    dfs(child)
+    max_s = max(max_s, salaries[child])
+    min_s = min(min_s, salaries[child])
+
+  salaries[s] = max_s + min_s + 1
+
+dfs(1)
+
+# print(salaries)
+print(salaries[1])
--- tessoku-book/B20/main.py
+++ tessoku-book/B20/main.py
@@ -2,27 +2,23 @@
 S = input()
 T = input()
 
-if len(T) > len(S):
-  S,T=T,S
-
 dp = []
-for i in range(len(S) + 1):
-  dp.append([1e18] * (len(T) + 1))
+for i in range(1 + len(S)):
+  d = [0] * (1 + len(T))
+  dp.append(d)
 
-for i in range(len(S) + 1):
+for i in range(1 + len(S)):
   dp[i][0] = i
-for i in range(len(T) + 1):
+for i in range(1 + len(T)):
   dp[0][i] = i
 
 for i in range(1, len(S) + 1):
   for j in range(1, len(T) + 1):
-    s = S[i - 1]
-    t = T[j - 1]
-    cost = 0 if s == t else 1
+    cost = 0 if S[i - 1] == T[j - 1] else 1
     dp[i][j] = min(
-      dp[i - 1][j - 1] + cost,
       dp[i][j - 1] + 1,
-      dp[i - 1][j] + 1
+      dp[i - 1][j] + 1,
+      dp[i - 1][j - 1] + cost
     )
 
-print(dp[len(S)][len(T)])  
+print(dp[len(S)][len(T)])
