bfa17aade4f1c60cb8a9b09e5bc6993571f469b7
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-09 20:47:19 +0900

    逆演算

--- tessoku-book/A24/main.py
+++ tessoku-book/A24/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+from bisect import bisect_left
+
+N = int(input())
+A = list(map(int, input().split()))
+X = 500000
+
+L = [1e18] * (X)
+dp = [0] * (1 + X)
+
+for i in range(N):
+  idx = bisect_left(L, A[i])
+
+  L[idx] = min(L[idx], A[i])
+  dp[A[i]] = idx + 1
+
+# print(dp)
+print(max(dp))
