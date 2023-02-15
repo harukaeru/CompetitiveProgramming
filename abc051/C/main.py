0daae51df04ef7f35e43d75280dc33182cadd346
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-15 22:11:32 +0900

    並べる

--- abc051/C/main.py
+++ abc051/C/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3.8
+sx,sy,tx,ty=map(int, input().split())
+
+ans = ''
+
+ans += 'U' * (ty - sy)
+ans += 'R' * (tx - sx)
+ans += 'D' * (ty - sy)
+ans += 'L' * (tx - sx)
+
+ans += 'L'
+ans += 'U' * (ty - sy)
+ans += 'UR'
+ans += 'R' * (tx - sx)
+ans += 'D'
+
+ans += 'R'
+ans += 'D' * (ty - sy)
+ans += 'DL'
+ans += 'L' * (tx - sx)
+ans += 'U'
+
+print(ans)
