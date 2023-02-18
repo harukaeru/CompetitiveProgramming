9e47098b45f37453a9780344a0973e096545357f
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-18 21:59:55 +0900

    A[n]+A[n+1]>x

--- abc048/C/main.py
+++ abc048/C/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+N,x=map(int, input().split())
+A = list(map(int, input().split()))
+
+l = 0
+r = 1
+tot = 0
+for i in range(N - 1):
+  if A[i] + A[i + 1] > x:
+    rem = min(A[i + 1], A[i] + A[i + 1] - x)
+    if A[i + 1] > 0:
+      A[i + 1] -= rem
+      tot += rem
+
+    rem = A[i] + A[i + 1] - x
+    tot += rem
+    A[i] -= rem
+  # print(A)
+print(tot)
