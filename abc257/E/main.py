70563bc22d47284f3f2e34fdd321e8e5bba66db3
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-21 23:22:50 +0900

    ad-hoc

--- abc257/E/main.py
+++ abc257/E/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3.8
+N = int(input())
+C = list(map(int, input().split()))
+
+length = N // min(C)
+ans = ''
+
+for i in range(length):
+  rest = length - i - 1
+
+  for j in range(8, -1, -1):
+    if min(C) * rest + C[j] <= N:
+      ans += str(j + 1)
+      N -= C[j]
+      break
+
+print(ans)
