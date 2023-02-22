9c5bdfbae6bd163f1dc72cc51802643a4774c5d1
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-22 21:43:53 +0900

    mex

--- abc194/E/main.py
+++ abc194/E/main.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3.8
+N,M=map(int, input().split())
+A = list(map(int, input().split()))
+
+count = [0] * N
+for i in range(M):
+  count[A[i]] += 1
+
+found = [0] * N
+for i, c in enumerate(count):
+  if c == 0:
+    found[i] = 1
+    break
+
+for i in range(M, N):
+  count[A[i - M]] -= 1
+  count[A[i]] += 1
+
+  found[A[i - M]] |= count[A[i - M]] == 0
+
+# print(found)
+for i in range(N):
+  if found[i] == 1:
+    print(i)
+    break
