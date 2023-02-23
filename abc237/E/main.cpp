9afff7b3c17a4062e4a2a56264186b825ccec679
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-24 01:54:16 +0900

    ベルマンフォードからのダイクストラ

--- abc237/E/main.cpp
+++ abc237/E/main.cpp
@@ -1,9 +1,49 @@
 #include <bits/stdc++.h>
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-
 using namespace std;
 
 int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> H(N);
+  for (int i = 0; i < N; i++) {
+    cin >> H[i];
+  }
+
+  vector<unordered_map<int, long long>> G(N);
+  for (int i = 0; i < M; i++) {
+    int u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+    if (H[u] <= H[v]) {
+      swap(u, v);
+    }
+    if (!G[u].count(v)) {
+      G[u][v] = -(H[u] - H[v]);
+      G[v][u] = 2 * (H[u] - H[v]);
+    }
+  }
+
+  vector<long long> dists(N, 1e18);
+  dists[0] = 0;
+  priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+  pq.push({0, 0});
+  while (!pq.empty()) {
+    auto [score, u] = pq.top();
+    pq.pop();
+    if (dists[u] > score) {
+      continue;
+    }
+    for (auto [v, v_cost] : G[u]) {
+      if (dists[u] + v_cost < dists[v]) {
+        dists[v] = dists[u] + v_cost;
+        pq.push({dists[v], v});
+      }
+    }
+  }
+
+  cout << -*min_element(dists.begin(), dists.end()) << endl;
+
   return 0;
 }
--- abc237/E/main.py
+++ abc237/E/main.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3.8
+from collections import defaultdict, deque
+import heapq
+
+
+N,M=map(int, input().split())
+H = list(map(int, input().split()))
+
+G = {}
+for i in range(M):
+  u,v=map(int, input().split())
+  u -= 1
+  v -= 1
+  if H[u] <= H[v]:
+    v,u = u,v
+
+  d = H[u] - H[v]
+  G.setdefault(u, {}).setdefault(v, 0)
+  G.setdefault(v, {}).setdefault(u, d)
+
+# print(G)
+dists = [1e18] * N
+dists[0] = 0
+
+pq = []
+pq.append((0, 0))
+while pq:
+  score, u = heapq.heappop(pq)
+  if dists[u] > score:
+    continue
+
+  for v, v_cost in G.get(u, {}).items():
+    if dists[u] + v_cost < dists[v]:
+      dists[v] = dists[u] + v_cost
+      heapq.heappush(pq, (dists[v], v))
+
+# print(dists)
+ans = 0
+for i in range(N):
+  ans = min(ans, dists[i] - (H[0] - H[i]))
+# print(dists)
+print(-ans)
