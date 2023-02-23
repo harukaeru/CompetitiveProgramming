4b81296de4cd8f2422e428212ddd5c644af8cb4d
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-23 18:15:36 +0900

    チェック

--- abc194/C/main.py
+++ abc194/C/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+s = 0
+for i in range(N):
+  a = A[i]
+  s += a ** 2
+
+s *= (N - 1)
+tot = sum(A)
+x = 0
+for i in range(N):
+  a = A[i]
+  x += a * (tot - a)
+
+s -= x
+print(s)
