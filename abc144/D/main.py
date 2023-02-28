449043eb549254a4823f5268c7aacbb78949be09
harukaeru <nekoneko.myaomyao@gmail.com>
2023-02-28 23:16:08 +0900

    atan2で

--- abc144/D/main.py
+++ abc144/D/main.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python3.8
-from math import atan2, cos, pi, radians, sin, tan
+from math import atan2, cos, degrees, pi, radians, sin, tan
+import math
 
 a,b,x=map(int, input().split())
 
-def calc(theta):
-  theta = radians(theta)
-  criteria = atan2(b, a)
-  if theta >= criteria:
-    return b * b * tan(pi / 2 - theta) * a / 2
+s = x / a
+if (a * a * b) / 2 <= x:
+  t = a * b - s
+  r = atan2(a, 2 * t / a)
+else:
+  r = atan2(2 * s / b, b)
 
-  si = sin(theta)
-  co = cos(theta)
-
-  tot = (b ** 2) * si * co / 2 + a * b * co * co
-  s = a * a * si * co / 2
-  k = ((b * co - a * si) / (a * co)) ** 2 * s
-
-  vv = tot - (s + k)
-  return vv * a
-
-l = 0
-r = 90
-# for i in range(90):
-#   print(i, '->', calc(i))
-while r - l >= 0.000000001:
-  mid = (l + r) / 2
-  v = calc(mid)
-  # print(mid, '->', v)
-  if v >= x:
-    l = mid
-  else:
-    r = mid
-# print(calc(45))
-# print(l)
-print((l + r) / 2)
+print(90 - degrees(r))
