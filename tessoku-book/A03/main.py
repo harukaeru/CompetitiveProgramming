7f25cd68de5a850b2595463c1b19165462b5880d
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-02 00:13:23 +0900

    アリバイ

--- tessoku-book/A03/main.py
+++ tessoku-book/A03/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3.8
+N,K=map(int, input().split())
+P = list(map(int, input().split()))
+Q = list(map(int, input().split()))
+
+for i in range(N):
+  for j in range(N):
+    if P[i] + Q[j] == K:
+      print('Yes')
+      exit()
+print('No')
