40a1068f48876726a7ce3cf669a96b04e1013062
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-04 20:05:11 +0900

    アリバイ

--- tessoku-book/A28/main.py
+++ tessoku-book/A28/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3.8
+N = int(input())
+mod = 10000
+v = 0
+for i in range(N):
+  t,a = input().split()
+  a = int(a)
+  if t == '+':
+    v += a
+  elif t == '-':
+    v -= a
+  elif t == '*':
+    v *= a
+  v %= mod
+  print(v)
+
