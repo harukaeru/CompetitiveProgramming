c071fcac49a00eb81b769022dc0167ff3677c4a9
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-05 23:41:54 +0900

    連結成分のチェック

--- abc292/D/main.py
+++ abc292/D/main.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3.8
+from collections import defaultdict, deque
+
+
+N,M=map(int, input().split())
+G = defaultdict(list)
+for i in range(M):
+  u,v=map(int, input().split())
+  u -= 1
+  v -= 1
+  if u == v:
+    G[u].append(v)
+  else:
+    G[u].append(v)
+    G[v].append(u)
+
+
+seen = set()
+for i in range(N):
+  if i in seen:
+    continue
+
+  q = deque()
+  q.append(i)
+  v_counter = 0
+  e_counter = 0
+
+  while q:
+    v = q.popleft()
+    if v in seen:
+      continue
+    v_counter += 1
+
+    for nex in G[v]:
+      if nex not in seen:
+        e_counter += 1
+        q.append(nex)
+    seen.add(v)
+  if v_counter != e_counter:
+    print('No')
+    exit()
+  # print(v_counter, e_counter)
+print('Yes')
