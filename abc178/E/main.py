aad23bee84f533943ecb1dad328063b1ec3503a0
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-07 23:27:51 +0900

    45度回転

--- abc178/E/main.py
+++ abc178/E/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+N = int(input())
+Z = []
+W = []
+for i in range(N):
+  x, y = map(int, input().split())
+  Z.append(x + y)
+  W.append(x - y)
+
+print(max(max(Z) - min(Z), max(W) - min(W)))
