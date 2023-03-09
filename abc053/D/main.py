b7fa608c20f04b34361102a6ad15aef7600fe8fd
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-09 22:25:55 +0900

    偶奇性てきなやつ

--- abc053/D/main.py
+++ abc053/D/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+counter = [0] * (max(A) + 1)
+for i in range(N):
+  counter[A[i]] += 1
+
+tot = 0
+two_cnt = 0
+for j in range(max(A) + 1):
+  if counter[j] == 0:
+    counter[j] = 0
+  elif counter[j] % 2 == 0:
+    counter[j] = 2
+    two_cnt += 1
+  elif counter[j] % 2 != 0:
+    counter[j] = 0
+    tot += 1
+
+tot += (two_cnt // 2) * 2
+print(tot)
