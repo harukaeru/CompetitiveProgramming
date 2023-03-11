d96e8c900189fced75e04941d1484e57a60541a9
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-11 19:58:35 +0900

    座圧

--- abc036/C/main.py
+++ abc036/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = []
+for i in range(N):
+  A.append(int(input()))
+
+B = set(A)
+B = sorted(list(B))
+conv = dict()
+for i, b in enumerate(B):
+  conv[b] = i
+
+for i in range(N):
+  print(conv[A[i]])
