74509547fb125f7d4015c68c869f9af590a2182e
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-14 20:41:40 +0900

    ダブリング

--- abc293/E/main.py
+++ abc293/E/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3.8
+from math import gcd
+
+
+A,X,M=map(int, input().split())
+
+# def mod_pow(x: int, n: int):
+#     bi = str(format(n, "b"))
+#     res = 1
+#     a = x
+#     print('bi', bi)
+#     for i in range(len(bi)):
+#         if n >> i & 1:
+#             res = (res * a) % M
+#         a = (a * a) % M
+#     return res
+if A == 1:
+  print(X % M)
+  exit()
+
+u = (pow(A, X, M * (A - 1)) - 1)
+d = (A- 1)
+
+print((u // d) % M)
