b436ecefc6874bdcb6d054f7c1b64a5201e2af84
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-15 23:09:47 +0900

    解説AC。まだよくわかってない

--- abc164/D/main.py
+++ abc164/D/main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3.8
+S = input()
+S = S[::-1]
+
+ans = 0
+mods = [0] * 2019
+mods[0] = 1
+current = 0
+x = 1
+for s in S:
+  # print('s', s)
+  current = (current + x * int(s)) % 2019
+  ans += mods[current % 2019]
+  mods[current % 2019] += 1
+  x = x * 10 % 2019
+  # print('current', current)
+print(mods)
+print(ans)
