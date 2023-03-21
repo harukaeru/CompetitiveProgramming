d925bd6e95563e1036960b58c0e237527ac7797f
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-21 21:36:16 +0900

    ダブリング

--- tessoku-book/B29/main.py
+++ tessoku-book/B29/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+a,b=map(int, input().split())
+mod = 1000000007
+
+bb = bin(b)[2:]
+ans = 1
+p = a
+for i in range(len(bb)):
+  if (b & 1 << i) != 0:
+    ans *= p
+    ans %= mod
+  p *= p
+  p %= mod
+print(ans)
