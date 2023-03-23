f137d0953441aad41ec7c9c441db5a92dc6a98cc
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-23 21:26:26 +0900

    包除原理2

--- tessoku-book/B31/main.py
+++ tessoku-book/B31/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+N = int(input())
+
+c3 = N // 3
+c5 = N // 5
+c7 = N // 7
+
+c15 = N // 15
+c21 = N // 21
+c35 = N // 35
+
+c105 = N // 105
+
+print(c3 + c5 + c7 - (c15 + c21 + c35) + c105)
