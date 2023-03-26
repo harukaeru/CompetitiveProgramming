9b7094556c85b73e97cb16eca491170c9193b0aa
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-26 23:20:38 +0900

    カウント

--- abc037/C/main.py
+++ abc037/C/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N,K=map(int, input().split())
+A = list(map(int, input().split()))
+
+C = [0] * (N + 1)
+
+for i in range(N - K + 1):
+  C[i] += 1
+  C[i + K] -= 1
+
+B = list(itertools.accumulate(C))
+# print(B)
+
+tot = 0
+for i in range(N):
+  tot += A[i] * B[i]
+print(tot)
--- abc295/E/main.py
+++ abc295/E/main.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python3.8
+N,M,K=map(int, input().split())
+A = list(map(int, input().split()))
+
