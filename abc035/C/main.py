0c15fc0b5b95dc0917afd39ec320eb5b3028bc10
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-27 22:52:42 +0900

    累積和

--- abc035/C/main.py
+++ abc035/C/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3.8
+import itertools
+
+
+N,Q=map(int, input().split())
+states = [0] * (N + 1)
+for i in range(Q):
+  l,r=map(int, input().split())
+  states[l - 1] += 1
+  states[r] -= 1
+
+states = list(itertools.accumulate(states))
+
+
+states = [str(s % 2) for s in states][:N]
+print(''.join(states))
+
+
+
