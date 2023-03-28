aec7b57deb2e9a51ac03bf756d826ce698c5e31b
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-28 21:59:32 +0900

    bit全探索

--- abc015/C/main.py
+++ abc015/C/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.8
+N,K=map(int, input().split())
+T = []
+for i in range(N):
+  ts = list(map(int, input().split()))
+  T.append(ts)
+
+for i in range(K ** N):
+  k = i
+  p = None
+  for j in range(N):
+    if p is None:
+      p = T[j][k % K]
+    else:
+      p ^= T[j][k % K]
+    k //= K
+  if p == 0:
+    print('Found')
+    exit()
+print('Nothing')
