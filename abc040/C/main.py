548bca88e2f2af6296f60aefbea3974bab3a2012
harukaeru <nekoneko.myaomyao@gmail.com>
2023-03-31 23:01:37 +0900

    簡単な動的計画法

--- abc040/C/main.py
+++ abc040/C/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [1e18] * N
+dp[0] = 0
+
+for i in range(N):
+  if i + 1 < N:
+    dp[i + 1] = min(dp[i + 1], dp[i] + abs(A[i + 1] - A[i]))
+  if i + 2 < N:
+    dp[i + 2] = min(dp[i + 2], dp[i] + abs(A[i + 2] - A[i]))
+print(dp[N - 1])
+
