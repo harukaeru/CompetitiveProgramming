ffec9d1bac01882d3c147ab2268dd9287d6167ad
harukaeru <nekoneko.myaomyao@gmail.com>
2023-04-03 21:17:34 +0900

    ABC296 2

--- abc296/B/main.py
+++ abc296/B/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+S = []
+for i in range(8):
+  S.append(input())
+
+for j in range(8):
+  for i in range(8):
+    if S[i][j] == '*':
+      print(chr(ord('a') + j) + str(7 - i + 1))
+      break
