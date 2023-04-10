def04ba971dc520c8c5a696e355d0bd05ec84ef6
harukaeru <nekoneko.myaomyao@gmail.com>
2023-04-10 22:31:40 +0900

    ゆりもどし

--- abc296/C/main.py
+++ abc296/C/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.8
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+A.sort()
+
+B = set(A)
+
+for i in range(N):
+  if A[i] + X in B:
+    print('Yes')
+    exit()
+print('No')
