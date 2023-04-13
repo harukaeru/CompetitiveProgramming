3327a09bf48a73041fc65a6072521bc157e1601c
harukaeru <nekoneko.myaomyao@gmail.com>
2023-04-13 23:08:31 +0900

    B問題

--- abc297/B/main.py
+++ abc297/B/main.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3.8
+S = input()
+
+x = None
+y = None
+for i in range(len(S)):
+  if S[i] == 'B':
+    if x is None:
+      x = i
+    else:
+      y = i
+
+inner = False
+cond2 = False
+
+for i in range(len(S)):
+  if S[i] == 'R':
+    if not inner:
+      inner = True
+    else:
+      inner = False
+  else:
+    if S[i] == 'K' and inner:
+      cond2 = True
+
+cond1 = x % 2 != y % 2
+
+if cond1 and cond2:
+  print('Yes')
+else:
+  print('No')
