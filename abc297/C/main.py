3518994a224aaaabed7c1e130e6ea5b21c22659a
harukaeru <nekoneko.myaomyao@gmail.com>
2023-04-15 23:53:23 +0900

    C

--- abc297/C/main.py
+++ abc297/C/main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3.8
+H,W=map(int, input().split())
+S = []
+for i in range(H):
+  S.append(list(input()))
+
+cnt = 0
+for i in range(H):
+  for j in range(W - 1):
+    if S[i][j] == 'T' and S[i][j + 1] == 'T':
+      S[i][j] = 'P'
+      S[i][j + 1] = 'C'
+      cnt += 1
+for i in range(H):
+  print(''.join(S[i]))
