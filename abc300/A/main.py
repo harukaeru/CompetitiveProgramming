e682e0d33889fb322d9f722f3f96ae77aeffd18c
harukaeru <nekoneko.myaomyao@gmail.com>
2023-09-20 01:12:54 +0900

    ひさびさのatcoder

--- abc300/A/main.py
+++ abc300/A/main.py
@@ -1,5 +1,1 @@
 #!/usr/bin/env python3.8
-N,A,B=map(int, input().split())
-C = list(map(int, input().split()))
-
-print(C.index(A + B) + 1)
--- abc300/C/main.py
+++ abc300/C/main.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3.8
+from collections import deque
+
+
+H,W=list(map(int,input().split()))
+C = []
+for i in range(H):
+  row = list(input())
+  C.append(row)
+
+q = deque()
+ans = [0] * min(H, W)
+
+seen = set()
+
+for i in range(H):
+  for j in range(W):
+    if C[i][j] == '.':
+      continue
+
+    if (i, j) in seen:
+      continue
+
+    q.append([(i, j), 1])
+    # print((i, j))
+    size = 1
+    while len(q) > 0:
+      v, s = q.popleft()
+      seen.add((v[0], v[1]))
+
+      downright = [v[0] + 1, v[1] + 1]
+
+      if downright[0] >= H or downright[1] >= W or C[downright[0]][downright[1]] == '.':
+        break
+      # print('  down ', downright)
+
+      size = s + 1
+      q.append([downright, size])
+
+    size //= 2
+    if size == 0:
+      continue
+    # print((i, j), size)
+    ans[size - 1] += 1
+
+print(*ans)
+
