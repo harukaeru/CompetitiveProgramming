8319b20a20dc2c32844c1e1458dbe2cac6ddd089
harukaeru <nekoneko.myaomyao@gmail.com>
2026-06-19 15:06:18 +0900

    2026/06/19

--- abc460/A/main.py
+++ abc460/A/main.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3.8
+N, M = list(map(int, input().split()))
+
+cnt = 0
+while M > 0:
+  M = N % M
+  cnt += 1
+
+print(cnt)
--- abc460/B/main.py
+++ abc460/B/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.8
+T = int(input())
+
+for i in range(T):
+  case = list(map(int, input().split()))
+  x1, y1, r1 = case[0], case[1], case[2]
+  x2, y2, r2 = case[3], case[4], case[5]
+
+  dd = ((x2 - x1) ** 2) + ((y2 - y1) ** 2)
+  rr = (r1 + r2) ** 2
+
+  rr2 = (max(r1, r2) - min(r1, r2)) ** 2
+  
+  print('Yes' if rr2 <= dd and dd <= rr else 'No')
